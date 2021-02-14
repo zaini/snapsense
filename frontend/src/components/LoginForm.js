189d9d53390ce04b97d7a333e0c4bbd691df1991
Zaini <alimzaini@outlook.com>
2021-02-14 13:22:10 +0000

    Add react hook form

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Button } from "@material-ui/core";
+import { useForm } from 'react-hook-form';
 
 const LoginForm = (props) => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <input></input>
-      <input></input>
-      {/* <button>click</button> */}
-      <Button variant="contained">Login</Button>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
+      <input type="text" placeholder="Last name" name="Last name" ref={register({required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="text" placeholder="Password" name="Password" ref={register({required: true, max: 18, min: 0})} />
+
+      <input type="submit" />
+    </form>
   );
 };
 
