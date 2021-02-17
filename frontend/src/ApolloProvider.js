7f55febf321164f4000c0b420bd390242a5db10f
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-17 01:30:54 +0000

    Connect the front-end to the back-end using ApolloPovider

---
+++ frontend/src/ApolloProvider.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import App from './App';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';	
+
+const backendLink = createHttpLink({
+    uri: process.env.BACKEND_URL || 'http://192.168.0.31:5000/'
+});
+
+
+// TODO: Once login is setup, use setContext to create an auth context
+
+
+const client = new ApolloClient({
+	link: backendLink,
+	cache: new InMemoryCache(),
+	connectToDevTools: true
+});
+
+
+export default () => {
+	return (
+		<ApolloProvider client={client}>
+			<App/>
+		</ApolloProvider>
+	);
+};
