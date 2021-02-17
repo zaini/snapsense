f55a1af2639d81f8842814715c86c616654977d6
Zaini <alimzaini@outlook.com>
2021-02-17 21:04:00 +0000

    Add Sequelize model for Patient

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,20 +1,25 @@
+const { DataTypes } = require("sequelize");
+
 // Example Patient model from database to be used in the resolvers
 // Potentially using https://sequelize.org/
 
 // example:
 
-module.exports = (sequelize, Sequelize) => {
-  const Patient = sequelize.define("Patient", {
-    firstName: {
-      type: Sequelize.STRING,
+const patient_model = (sequelize) => {
+  const patient = sequelize.define("patient", {
+    patient_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    lastName: {
-      type: Sequelize.STRING,
-    },
-    email: {
-      type: Sequelize.STRING,
-    },
+    patient_fname: DataTypes.STRING,
+    patient_lname: DataTypes.STRING,
+    patient_email: DataTypes.STRING,
+    patient_password: DataTypes.STRING,
+    patient_date_joined: DataTypes.DATE,
   });
 
-  return Patient;
+  return patient;
 };
+
+module.exports = patient_model;
