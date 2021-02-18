c13589089085a8262270999fb15126d51775dc55
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-18 14:39:42 +0000

    Remove unessacary comments

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,6 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-// Database connection parameters
-// const DATABASE_CONNECTION_URL = process.env.DATABASE_CONNECTION;
-// const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
-// const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
-// const DATABASE_SCHEMA = process.env.DATABASE_SCHEMA;
-
 // MySQL and Sequelize Connection
 const sequelize = new Sequelize(
   process.env.DATABASE_SCHEMA,
