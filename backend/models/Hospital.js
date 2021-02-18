68cf2ac42f3cab405ef6b0a1875dad6a9912cdf0
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-18 14:41:34 +0000

    Refactor Hospital model

--- backend/models/Hospital.js
+++ backend/models/Hospital.js
@@ -3,15 +3,16 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
 
 const Hospital = () => {
-  return sequelize.define("hospital", {
+  const hospital = sequelize.define("hospital", {
     hospital_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
     hospital_name: DataTypes.STRING,
-    hospital_contact_email: DataTypes.STRING
+    hospital_contact_email: DataTypes.STRING,
   });
+  return hospital;
 };
 
 module.exports = Hospital();
