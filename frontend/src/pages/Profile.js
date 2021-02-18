4b28666998b61b6454f3489305157ec3214fa306
Soyoung Park <p.soyoung63@gmail.com>
2021-02-19 01:06:43 +0900

    Add password for input

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -4,7 +4,7 @@ import UserInfo from "../components/UserInfo.js";
 const Profile = () => {
   return (
     <div>
-      <UserInfo firstName="first name" lastName="last name" email="aaa@email.com" />
+      <UserInfo firstName="first name" lastName="last name" email="aaa@email.com" password = "abc******"/>
     </div>
   );
 };
