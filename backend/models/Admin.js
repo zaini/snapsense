70206b7c685b6a9ddbb118bc2d63596c677dc25c
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-18 14:40:30 +0000

    Refactor Admin model
    -Refactored Admin model to add relationships with Hospital

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,20 +1,23 @@
-// const { DataTypes } = require("sequelize");
+const { DataTypes } = require("sequelize");
+const Hospital = require("./Hospital");
 
-// const sequelize = require("../db");
+const sequelize = require("../db");
 
-// const Admin = () => {
-//   return sequelize.define("admin", {
-//     admin_id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     admin_fname: DataTypes.STRING,
-//     admin_lname: DataTypes.STRING,
-//     admin_email: DataTypes.STRING,
-//     admin_password: DataTypes.STRING,
-//     admin_date_joined: DataTypes.DATE,
-//   });
-// };
+const Admin = () => {
+  const admin = sequelize.define("admin", {
+    admin_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    admin_fname: DataTypes.STRING,
+    admin_lname: DataTypes.STRING,
+    admin_email: DataTypes.STRING,
+    admin_password: DataTypes.STRING,
+    admin_date_joined: DataTypes.DATE,
+  });
+  admin.belongsTo(Hospital, { foreignKey: "hospital_id" });
+  return admin;
+};
 
-// module.exports = Admin();
+module.exports = Admin();
