706b70e6f312706a3e60398f794ed3a7d1595010
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-02-19 17:49:58 +0000

    Add right menu options for different users + temporary page links

--- frontend/src/model/navBarModels.js
+++ frontend/src/model/navBarModels.js
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
+
 export const navBarOptions = {
-  admin: ["account info", "logout"],
-  patient: ["history", "log out"],
-  doctor: ["Patient list", "log out"],
+  admin: ["Account info", "Doctors", "Patients", "Invite patient", "Log out"],
+  patient: ["Account info", "History", "Patient upload", "Log out"],
+  doctor: ["Account info", "Patients", "Invite patient", "Latest Uploads", "Log out"],
 };
+
+//TODO: Change the links to the appropriate pages once created
 export const navBarLinks = {
-  "account info": "./accountInfo",
-  "log out": "./home",
+  "Account info": <Link to="/">Home</Link>,
+  "Doctors": <Link to="/">Home</Link>,
+  "Patients": <Link to="/">Home</Link>,
+  "Invite patient": <Link to="/">Home</Link>,
+  "History": <Link to="/">Home</Link>,
+  "Latest uploads": <Link to="/">Home</Link>,
+  "Log out": <Link to="/">Home</Link>,
 };
 
