da24e99f3f305bfaa68be9c6028d636e1841c1bf
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-19 23:46:52 +0000

    Add example query

--- backend/graphql/resolvers/admins.js
+++ backend/graphql/resolvers/admins.js
@@ -1,10 +1,17 @@
 const { Admin } = require("../../models/index.js");
 
 module.exports = {
-    Query: {
-
+  Query: {
+    async getAdmins() {
+      try {
+        const admins = await Admin.findAll();
+        return admins;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
-    Mutation: {
-        
-    },
+  },
+  Mutation: {
+    
+  },
 };
--- backend/graphql/resolvers/doctors.js
+++ backend/graphql/resolvers/doctors.js
@@ -1,10 +1,17 @@
 const { Doctor } = require("../../models/index.js");
 
 module.exports = {
-    Query: {
-
+  Query: {
+    async getDoctors() {
+      try {
+        const doctors = await Doctor.findAll();
+        return doctors;
+      } catch (error) {
+        throw new Error(error);
+      }
     },
-    Mutation: {
-        
-    },
+  },
+  Mutation: {
+    
+  },
 };
