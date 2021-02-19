aaa94d69aa30bf85b790507283f5639f56556dc7
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-02-19 15:52:45 +0300

    Delete components for refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,4 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from "./pages/Home.js";
-import About from "./pages/About.js";
-import LoginPage from "./pages/LoginPage.js";
 
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        {/*<Switch>
-          <Route path='/' exact component={LoginPage} />
-          <Route path='/services' component={SomePage} />
-          <Route path='/products' component={SomePage} />
-          <Route path='/login' component={SomePage} /> 
-        </Switch>*/}
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-        </Switch>
-      </Router>
-
-   
-    </>
-  );
-}
 
 export default App;
