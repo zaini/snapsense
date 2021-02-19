07a1328263b194508964bdd326ed7d47874e7cb6
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-02-19 13:10:32 +0000

    Add basic drawer with icons

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -9,13 +9,13 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
-// import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-// import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import InboxIcon from "@material-ui/icons/MoveToInbox";
-// import MailIcon from "@material-ui/icons/Mail";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 
 const drawerWidth = 240;
 
@@ -114,7 +114,7 @@ function NavBar() {
           <Button color="inherit">Log In</Button>
         </Toolbar>
       </AppBar>
-      {/* <Drawer
+      <Drawer
         className={classes.drawer}
         variant="persistent"
         anchor="left"
@@ -143,7 +143,7 @@ function NavBar() {
             </ListItem>
           ))}
         </List>
-      </Drawer> */}
+      </Drawer>
     </div>
   )
 }
