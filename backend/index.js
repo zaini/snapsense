ca64a2321c7620febe96b207c91a3296ca36177e
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-02-19 15:10:42 +0400

    Add Test Code for S3 and Update SDK Version
    Add Future based code for S3 with credentials stored in ENV.
    Bucket Name, Region stored on index.js
    Update SDK Version, only include S3 services.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,40 @@
 const { ApolloServer } = require("apollo-server");
-
+require("dotenv").config();
 const typeDefs = require("./graphql/typeDefs/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
+//////AWS//////
+const path = require("path");
+const fs = require("fs");
+//////AWS//////
 const port = process.env.PORT || 5000;
+
+
+//////AWS//////
+const { S3Client, PutObjectCommand  } = require("@aws-sdk/client-s3");
+const s3 = new S3Client({ region: process.env.AWS_REGION });
+
+const file = "toupload/aa.jpg";
+
+const uploadParams = { Bucket: "snapsensebucket", ACL:'public-read' };
+uploadParams.Key = path.basename(file);
+
+const command = new PutObjectCommand(uploadParams);
+
+// promise method.
+s3
+  .send(command)
+  .then((data) => {
+    console.log("Success", data);
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  })
+  .finally(() => {
+    // finally.
+  });
+
+//////AWS//////
 
 // GraphQL Apollo Connection
 const server = new ApolloServer({
