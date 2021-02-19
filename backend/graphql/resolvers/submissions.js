9d1e73274792cc8efc62a52b5e594f32ff2e9331
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-19 23:58:35 +0000

    Add getSubmission query that retrieves all submissions
    This was added to check if GraphQL would return 'null' for the doctor_id if a Submission is not
    associated with a Doctor, which does happen.

--- backend/graphql/resolvers/submissions.js
+++ backend/graphql/resolvers/submissions.js
@@ -2,7 +2,14 @@ const { Submission } = require("../../models/index.js");
 
 module.exports = {
   Query: {
-
+    async getSubmissions() {
+      try {
+        const submissions = await Submission.findAll();
+        return submissions;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
   Mutation: {
     
