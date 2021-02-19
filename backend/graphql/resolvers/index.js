e99d86d8dc2787339d1f28dcbee848eba88349ab
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-19 23:07:21 +0000

    Update index.js in graphql resolvers to export the mutations and queries of all the models

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,11 +1,27 @@
+const hospitalResolvers = require("./hospitals");
+const adminResolvers = require("./admins");
+const doctorResolvers = require("./doctors");
+const patientResolvers = require("./patients");
+const submissionResolvers = require("./submissions");
 const imageResolvers = require("./images");
 
 module.exports = {
   Mutation: {
+    ...hospitalResolvers.Mutation,
+    ...adminResolvers.Mutation,
+    ...doctorResolvers.Mutation,
+    ...patientResolvers.Mutation,
+    ...submissionResolvers.Mutation,
     ...imageResolvers.Mutation,
   },
 
   Query: {
+    ...hospitalResolvers.Query,
+    ...adminResolvers.Query,
+    ...doctorResolvers.Query,
+    ...patientResolvers.Query,
+    ...submissionResolvers.Query,
+    ...imageResolvers.Query,
     helloWorld() {
       return "Hello World";
     },
