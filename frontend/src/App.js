6e62fda90f99a38e6e67634e163bee87599527eb
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-02-19 11:02:37 +0300

    Add basic nabvar, buttons style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from './components/Navbar';
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
-import Profile from "./pages/Profile.js";
 import LoginPage from "./pages/LoginPage.js";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-
-        <hr />
+    <>
+      <Router>
+        <Navbar />
+        {/*<Switch>
+          <Route path='/' exact component={LoginPage} />
+          <Route path='/services' component={SomePage} />
+          <Route path='/products' component={SomePage} />
+          <Route path='/login' component={SomePage} /> 
+        </Switch>*/}
 
         <Switch>
           <Route exact path="/">
@@ -29,15 +23,14 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
           <Route path="/login">
             <LoginPage />
           </Route>
         </Switch>
-      </div>
-    </Router>
+      </Router>
+
+   
+    </>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import * as FaIcons from "react-icons/fa"; 
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+    
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+  
+  return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to="/"className="navbar-logo">
+                        someLogo 
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='#' className='nav-links' onClick={closeMobileMenu}>
+                                <FaIcons.FaBars/>
+                            </Link>
+                        </li>                          
+                        <li className='nav-item'>
+                            <Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>LOGIN</Button>}
+                </div>
+            </nav>  
+        </>
+    )
+}
+
+export default Navbar
