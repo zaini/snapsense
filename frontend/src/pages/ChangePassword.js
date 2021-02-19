006150af594181dea84fb753edacc9c33cc11d7a
Soyoung Park <p.soyoung63@gmail.com>
2021-02-20 00:25:52 +0900

    Page for changing password

---
+++ frontend/src/pages/ChangePassword.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordForm from "../components/PasswordForm.js";
+
+const ChangePassword = () => {
+    return (
+        <div>
+            <PasswordForm newPassword="new****" />
+        </div>
+    );
+};
+
+export default ChangePassword;
