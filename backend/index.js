0f1be05b4dd09078f59fa5bf987a73e15cc91ce2
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-20 17:53:24 +0000

    Allow requests only from frontend (CORS)

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const typeDefs = require("./graphql/typeDefs/typeDefs");
@@ -16,7 +17,13 @@ const apolloServer = new ApolloServer({
   context: ({ req }) => ({ req }),
 });
 
-apolloServer.applyMiddleware({ app });
+// Allow requests only from the frontend
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
+apolloServer.applyMiddleware({ app, cors: corsOptions });
 
 app.listen(port, () => {
   console.log(`Server ready at http://localhost:${port}/`);
