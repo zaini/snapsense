a6d591580d15872ab771af4b6c27a9d40606aba6
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-02-20 09:35:12 +0300

    Add chained import, babel plugin, menu sequence

--- frontend/src/components/navBar/NavBar.js
+++ frontend/src/components/navBar/NavBar.js
@@ -1,20 +1,9 @@
 import { useState } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import { AppBar, Toolbar, Typography, Button, IconButton, makeStyles, useTheme, Drawer, 
+         List, Divider, ListItem, ListItemIcon, ListItemText }  from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from '@material-ui/icons/Close';
 import clsx from "clsx";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import { Link } from "react-router-dom";
 import {navBarOptions, navBarLinks, navBarIcons} from "../../model/navBarModels";
 
@@ -22,33 +11,27 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
+    display: 'flex',
   },
   appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
+    marginRight: drawerWidth,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  title: {
+    flexGrow: 1,
   },
   hide: {
-    display: "none",
+    display: 'none',
   },
   drawer: {
     width: drawerWidth,
@@ -58,30 +41,31 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: "flex-end",
+    justifyContent: 'flex-start',
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
+    transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    marginLeft: -drawerWidth,
+    marginRight: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create("margin", {
+    transition: theme.transitions.create('margin', {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
-    marginLeft: 0,
+    marginRight: 0,
   },
 }));
+
 
 function NavBar() {
   const classes = useStyles();
@@ -104,6 +88,9 @@ function NavBar() {
     <div>
       <AppBar position="static">
         <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+            Logo
+          </Typography>          
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -113,16 +100,12 @@ function NavBar() {
           >
           <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Logo
-          </Typography>
-          <Button color="inherit">Log In</Button>
         </Toolbar>
       </AppBar>
       <Drawer
         className={classes.drawer}
         variant="persistent"
-        anchor="left"
+        anchor="right"
         open={open}
         classes={{
           paper: classes.drawerPaper,
@@ -130,11 +113,7 @@ function NavBar() {
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "ltr" ? (
-              <ChevronLeftIcon />
-            ) : (
-              <ChevronRightIcon />
-            )}
+              <CloseIcon />
           </IconButton>
         </div>
         <Divider />
