785296e2c417e982fccb6f91c28e7524f7d79c83
EraDula <45733802+EraDula@users.noreply.github.com>
2021-02-20 22:14:19 +0000

    feat(navbar): added a functional navbar with menu list (#1)
    Added a basic navbar with a temporary logo and a menu list which changes options based on the type of user logged in. The menu list is hidden when a user is not logged in.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,36 +3,17 @@ import Home from "./pages/Home.js";
 import About from "./pages/About.js";
 import Profile from "./pages/Profile.js";
 import LoginPage from "./pages/LoginPage.js";
+import Navbar from "./components/navBar/Navbar.js";
 
 function App() {
   return (
     <Router>
+      <div><Navbar/></div>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-
         <hr />
 
         <Switch>
           <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/login">
             <LoginPage />
           </Route>
         </Switch>
---
+++ frontend/src/models/navbarModels.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import MailIcon from "@material-ui/icons/Mail";
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import GroupIcon from '@material-ui/icons/Group';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import HistoryIcon from '@material-ui/icons/History';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
+
+export const navbarOptions = {
+  admin: ["Account info", "Doctors", "Patients", "Invite doctor", "Log out"],
+  patient: ["Account info", "History", "Upload report", "Log out"],
+  doctor: ["Account info", "Patients", "Invite patient", "Latest uploads", "Log out"],
+};
+
+//TODO: Change the links to the appropriate pages once created
+export const navbarLinks = {
+  "Account info": <Link to="/account_info">Account info</Link>,
+  "Doctors": <Link to="/doctor_list">Doctors</Link>,
+  "Patients": <Link to="/patient_list">Patients</Link>,
+  "Invite patient": <Link to="/invite">Invite Patient</Link>,
+  "Invite doctor": <Link to="/invite">Invite Doctor</Link>,
+  "History": <Link to="/upload_list">History</Link>,
+  "Upload report": <Link to="/upload">Upload report</Link>,
+  "Latest uploads": <Link to="/latest_list">Latest uploads</Link>,
+  "Log out": <Link to="/">Logout</Link>,
+};
+
+export const navbarIcons = {
+  "Account info": <AccountBoxIcon />,
+  "Doctors": <LocalHospitalIcon />,
+  "Patients": <GroupIcon />,
+  "Upload report": <NoteAddIcon />,
+  "Invite patient": <ContactMailIcon />,
+  "Invite doctor": <ContactMailIcon />,
+  "History": <HistoryIcon />,
+  "Latest uploads": <DynamicFeedIcon />,
+  "Log out": <ExitToAppIcon />,
+}
+
