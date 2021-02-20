4a57d64005ec1d1f0783a8e3737c6885abb8e213
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-02-20 16:48:41 +0400

    Refactor isAuth.js and add Descriptive errors

--- backend/utils/isAuth.js
+++ backend/utils/isAuth.js
@@ -1,24 +1,26 @@
+const { AuthenticationError, ApolloError } = require("apollo-server");
 const { verify } = require("jsonwebtoken");
 require("dotenv").config();
+
 const ACCESS_TOKEN_SECRET_KEY = process.env.ACCESS_TOKEN_SECRET_KEY;
 
-const isAuth = (req, payload) => {
-  const authorization = req.headers["authorization"];
-
-  if (!authorization) {
-    throw new Error("Not authenticated");
+const isAuth = (context) => {
+  // Context = { ...headers }
+  const authHeader = context.req.headers["authorization"];
+  const needle = "Bearer ";
+  if (authHeader) {
+    const token = authHeader.split(needle)[1];
+    if (token) {
+      try {
+        const verifiedToken = verify(token, ACCESS_TOKEN_SECRET_KEY);
+        return verifiedToken;
+      } catch (error) {
+        throw new AuthenticationError("Invalid Login Token");
+      }
+    }
+    throw new ApolloError("Missing Authorization Token", "400");
   }
-
-  try {
-    const token = authorization.split(" ")[1];
-    console.log("token", token)
-    const new_payload = verify(token, ACCESS_TOKEN_SECRET_KEY);
-    payload = new_payload;
-    console.log(payload);
-    return payload;
-  } catch (error) {
-    throw new Error(error);
-  }
+  throw new ApolloError("Missing Authorization Header", "400");
 };
 
 module.exports = isAuth;
