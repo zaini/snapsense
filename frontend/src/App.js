91330ee2e4196fdd2aee3563bfdc2024398ad8a5
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-02-20 14:01:34 +0300

    Update footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,41 +10,19 @@ import LoginPage from "./pages/LoginPage.js";
 function App() {
   return (
     <Router>
+      {/* <div><Navbar/></div> */}
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-
         <hr />
 
         <Switch>
           <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/login">
             <LoginPage />
+            <>
+              <Footer />
+            </>
           </Route>
         </Switch>
       </div>
-
-      <>
-        <Footer />
-      </>
-
     </Router>
   );
 }
