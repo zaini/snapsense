229671fc4896ca945df2905b010bdec5597f47e3
zaini <alimzaini@outlook.com>
2021-02-20 07:53:56 +0000

    Add express functionality

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,21 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
+const express = require("express");
 require("dotenv").config();
 const typeDefs = require("./graphql/typeDefs/typeDefs");
 const resolvers = require("./graphql/resolvers");
-const imageUploader = require("./utils/imageUploader");
 
 const port = process.env.PORT || 5000;
-
-// imageUploader("./nasa.jpg");
+const app = express();
 
 // GraphQL Apollo Connection
-const server = new ApolloServer({
+const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
 });
 
-server.listen({ port }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+apolloServer.applyMiddleware({ app });
+
+app.listen(port, () => {
+  console.log(`Server ready at http://localhost:${port}/`);
 });
