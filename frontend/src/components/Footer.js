81c779789aa92bdcbe375ae0092d2edf523c7a9f
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-02-21 13:40:35 +0300

    Add footer style and copyright

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,26 +3,16 @@ import './Footer.css';
 import { Link } from 'react-router-dom';
 
 function Footer() {
+
   return (
-    <div className='footer-container'>
-      <div class='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>About</Link>
-            <Link to='/'>Terms of Service</Link>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-          </div>
-        </div>
-      </div>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <small class='website-rights'>SOMELOGO © 2021</small>
-        </div>
-      </section>
-    </div>
+    <footer>
+      <ul>
+        <li><Link to='/sign-up'>Privacy</Link></li>
+        <li><Link to='/'>Terms</Link></li>
+        <li><Link to='/'>Support</Link></li>
+      </ul>
+      <span>&copy;{new Date().getFullYear()} SNAPSENSE | All rights reserved</span>
+    </footer>
   );
 }
 
