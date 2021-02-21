8ecdcdd240763b3af016adb90d93b08943de5857
zaini <alimzaini@outlook.com>
2021-02-21 00:50:12 +0000

    Make the entire button a link instead of just text

--- frontend/src/components/navBar/Navbar.js
+++ frontend/src/components/navBar/Navbar.js
@@ -1,13 +1,31 @@
 import { useState, useEffect } from "react";
-import { AppBar, Toolbar, Typography, Button, IconButton, makeStyles, useTheme, Drawer, 
-         List, Divider, ListItem, ListItemIcon, ListItemText }  from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
-import CloseIcon from '@material-ui/icons/Close';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  makeStyles,
+  useTheme,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemSecondaryAction,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 import clsx from "clsx";
 import { Link } from "react-router-dom";
-import {navbarOptions, navbarLinks, navbarIcons} from "../../models/navbarModels";
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-
+import {
+  navbarOptions,
+  navbarLinks,
+  navbarIcons,
+  headerToRoute,
+} from "../../models/navbarModels";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 
 function Navbar() {
   const classes = useStyles();
@@ -15,7 +33,7 @@ function Navbar() {
   const [open, setOpen] = useState(false);
   useEffect(() => {
     setOpen(false);
-  },[]) 
+  }, []);
   const [accountType, setAccountType] = useState("admin");
   const [isLoggedIn, setLogedStatus] = useState(true);
   //const isLoggedIn = window.localStorage.getItem("authUser") ? true : false;
@@ -35,74 +53,75 @@ function Navbar() {
   };
 
   return (
-
     <ClickAwayListener
-    mouseEvent="onMouseDown"
-    touchEvent="onTouchStart"
-    onClickAway={handleClickAway}
+      mouseEvent="onMouseDown"
+      touchEvent="onTouchStart"
+      onClickAway={handleClickAway}
     >
       <div>
         <AppBar position="static">
           <Toolbar>
-            <Link to='/' className={classes.title}>
-                Logo
-            </Link>               
-            {isLoggedIn &&<IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              className={clsx(classes.menuButton, open && classes.hide)}
-            >
-            <MenuIcon />
-            </IconButton>}
+            <Link to="/" className={classes.title}>
+              Logo
+            </Link>
+            {isLoggedIn && (
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                className={clsx(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
           </Toolbar>
-      </AppBar>
-       <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="right"
-        open={open}
-        classes={{
-        paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            <CloseIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-          {menuList.map((text, index) => (
-            <ListItem button key={text} onClick = {() => setOpen(false)}>
-              <ListItemIcon>
-                {navbarIcons[text]}
-              </ListItemIcon>
-              <ListItemText primary={navbarLinks[text]} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-    </div>
-   </ClickAwayListener>
- );
-};
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="right"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {menuList.map((text, index) => (
+              <Link to={headerToRoute[text]}>
+                <ListItem button key={index} onClick={() => setOpen(false)}>
+                  <ListItemIcon>{navbarIcons[text]}</ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Drawer>
+      </div>
+    </ClickAwayListener>
+  );
+}
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -112,7 +131,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -122,12 +141,12 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   // content: {
   //   flexGrow: 1,
@@ -139,7 +158,7 @@ const useStyles = makeStyles((theme) => ({
   //   marginRight: -drawerWidth,
   // },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
