6a7a05cc4c492dce2fd9e82ccfe50fe8ad8d9d5e
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-22 20:12:18 +0000

    Connect the login component with the AuthProvider
    Allow the user to login, and save the user's state in the auth context

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { AuthContext } from "../context/auth";
 
 const LoginForm = (props) => {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  
+  const history = useHistory();
+  const context = useContext(AuthContext);
+  const [values, setValues] = useState({});
+
+  const { register, handleSubmit, setError, errors } = useForm();
+
+  const [login, { loading }] = useMutation(LOGIN_USER, {
+    update(_, { data: { login: userData } }) {
+      context.login(userData);
+      history.push("/");
+    },
+    onError(err) {
+      setError("error", { message: err.graphQLErrors[0].message });
+    },
+    variables: values,
+  });
+
+  const onSubmit = ({ email, password }) => {
+    setValues({ email, password, account_type: "ADMIN" });
+    login();
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="First name" name="First name" ref={register({required: true, maxLength: 80})} />
-      <input type="text" placeholder="Last name" name="Last name" ref={register({required: true, maxLength: 100})} />
-      <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
-      <input type="text" placeholder="Password" name="Password" ref={register({required: true, max: 18, min: 0})} />
+      {errors.error && <p severity="error">{errors.error.message}</p>}
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        name="password"
+        ref={register({ required: true, max: 18, min: 0 })}
+      />
 
       <input type="submit" />
     </form>
@@ -20,3 +52,11 @@ const LoginForm = (props) => {
 };
 
 export default LoginForm;
+
+const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!, $account_type: String!) {
+    login(email: $email, password: $password, account_type: $account_type) {
+      accessToken
+    }
+  }
+`;
