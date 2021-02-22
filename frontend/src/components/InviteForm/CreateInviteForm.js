4798cbacd39109d038242e186d5307a8065b0544
zaini <alimzaini@outlook.com>
2021-02-22 04:22:25 +0000

    Add react-hook-form to invite form

--- frontend/src/components/InviteForm/CreateInviteForm.js
+++ frontend/src/components/InviteForm/CreateInviteForm.js
@@ -6,31 +6,43 @@ import {
   Input,
   Button,
   Center,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
 
+// TODO add validation
 const CreateInviteForm = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors, formState } = useForm();
   const onSubmit = (data) => console.log(data);
   console.log(errors);
-
-  const [inviteEmail, setInviteEmail] = useState("");
 
   return (
     <Box p="7" borderWidth="1px" borderRadius="lg">
       <form onSubmit={handleSubmit(onSubmit)}>
+        <FormErrorMessage>
+          {errors.repeat_email && errors.repeat_email.message}
+        </FormErrorMessage>
         <FormControl id="email" isRequired>
-          <FormLabel>Email</FormLabel>
-          <Input placeholder="Email" />
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input name="email" placeholder="Email" ref={register} />
         </FormControl>
         <br />
         <FormControl id="repeat_email" isRequired>
-          <FormLabel>Repeat Email</FormLabel>
-          <Input placeholder="Repeat email" />
+          <FormLabel htmlFor="repeat_email">Repeat Email</FormLabel>
+          <Input
+            name="repeat_email"
+            placeholder="Repeat email"
+            ref={register}
+          />
         </FormControl>
         <br />
         <Center>
-          <Button mt={4} colorScheme="blue" type="submit">
+          <Button
+            mt={4}
+            colorScheme="blue"
+            type="submit"
+            isLoading={formState.isSubmitting}
+          >
             Send Invite
           </Button>
         </Center>
