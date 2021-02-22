54802fb7c5206d6d29ffbbb563820e8b0c90ce5e
Soyoung Park <p.soyoung63@gmail.com>
2021-02-22 16:56:25 +0900

    Add button for delete account

--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import { Container, List, ListItem, ListItemText, Grid, Paper } from "@material-ui/core";
+import { Container, List, ListItem, ListItemText, Grid, Paper, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core";
 
 const UserInfo = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Container className="UserInfo">
       <h1>Your information</h1>
+
       <Paper>
         <Grid container direction="row" alignItems="center" justify="center">
           <Grid item xs={12}>
@@ -38,7 +49,7 @@ const UserInfo = (props) => {
           </Grid>
 
           <Grid item xs={6}>
-            <Button component={Link} to="/changeemail" color="primary">
+            <Button style={{minWidth: '170px'}} variant="contained" component={Link} to="/changeemail" color="primary">
               Change email
         </Button>
           </Grid>
@@ -48,20 +59,51 @@ const UserInfo = (props) => {
               <ListItem>
                 <ListItemText
                   primary="Password"
-                  secondary={props.password.substring(0,3) + "******"}
+                  secondary={props.password.substring(0, 3) + "******"}
                 />
               </ListItem>
             </List>
           </Grid>
 
           <Grid item xs={6}>
-            <Button component={Link} to="/changepassword" color="primary">
+            <Button style={{minWidth: '170px'}} variant="contained" component={Link} to="/changepassword" color="primary">
               Change password
         </Button>
-
           </Grid>
         </Grid>
       </Paper>
+
+      <Grid item xs={6}>
+        <List>
+          <ListItem>
+            <Button color="primary" onClick={handleClickOpen}>
+              Delete account
+      </Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Are you sure you want to delete your account?"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  If you delete something will happen, maybe disadvantage you...
+          </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  Cancel
+          </Button>
+                <Button onClick={handleClose} color="primary" autoFocus>
+                  Continue
+          </Button>
+              </DialogActions>
+            </Dialog>
+          </ListItem>
+        </List>
+      </Grid>
+
     </Container >
   );
 }
