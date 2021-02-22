435b50c1f2a62c1fb1d2de828b6345bbcc6e899a
Soyoung Park <p.soyoung63@gmail.com>
2021-02-23 00:41:58 +0900

    Remove <paper> in EmailForm

--- frontend/src/components/EmailForm.js
+++ frontend/src/components/EmailForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
-import { Container, List, ListItem, Paper, FormControl, InputLabel, Input, FormHelperText } from "@material-ui/core";
+import { Container, List, ListItem, FormControl, InputLabel, Input, FormHelperText } from "@material-ui/core";
 import { useForm } from 'react-hook-form';
 
 
@@ -11,40 +11,37 @@ const EmailForm = (props) => {
 
     return (
         <Container>
-            <h1>Change Email</h1>
-            <Paper>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <List>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <List>
 
-                        <ListItem>
-                            <FormControl error={Boolean(errors.newEmail)}>
-                                <InputLabel htmlFor="newEmail">New Email</InputLabel>
-                                <Input id="newEmail" name="newEmail" type="text" inputRef={register({
-                                    required: true,
-                                    pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
-                                })} />
-                                <FormHelperText>
-                                    {errors.newEmail
-                                        && errors.newEmail.type === 'required'
-                                        && <span>Please enter your new email</span>}
-                                    {errors.newEmail
-                                        && errors.newEmail.type === 'pattern'
-                                        && <span>Please enter valid email</span>}
-                                </FormHelperText>
-                            </FormControl>
-                        </ListItem>
-                        <ListItem><Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                        >
-                            Save changes
+                    <ListItem>
+                        <FormControl error={Boolean(errors.newEmail)}>
+                            <InputLabel htmlFor="newEmail">New Email</InputLabel>
+                            <Input id="newEmail" name="newEmail" type="text" inputRef={register({
+                                required: true,
+                                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
+                            })} />
+                            <FormHelperText>
+                                {errors.newEmail
+                                    && errors.newEmail.type === 'required'
+                                    && <span>Please enter your new email</span>}
+                                {errors.newEmail
+                                    && errors.newEmail.type === 'pattern'
+                                    && <span>Please enter valid email</span>}
+                            </FormHelperText>
+                        </FormControl>
+                    </ListItem>
+                    <ListItem><Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Save changes
                     </Button>
-                        </ListItem>
+                    </ListItem>
 
-                    </List>
-                </form>
-            </Paper>
+                </List>
+            </form>
         </Container>
     );
 };
