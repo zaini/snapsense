e41720beb161014e7b9020e4839ab020b081a2f9
Soyoung Park <p.soyoung63@gmail.com>
2021-02-23 00:42:46 +0900

    Add actions for rendering forms - Allows forms rendered on the same page rather than loading new page

--- frontend/src/components/UserInfo.js
+++ frontend/src/components/UserInfo.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { Container, List, ListItem, ListItemText, Grid, Paper, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@material-ui/core";
+import PasswordForm from "../components/PasswordForm.js";
+import EmailForm from "../components/EmailForm.js";
 
 const UserInfo = (props) => {
   const [open, setOpen] = React.useState(false);
+  const [pwVisible, setPasswordFormVisible] = React.useState(false)
+  const [emailVisible, setEmailFormVisible] = React.useState(false)
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -14,94 +18,104 @@ const UserInfo = (props) => {
     setOpen(false);
   };
 
+  const handlePasswordForm = () => {
+    if (pwVisible === 'true') setPasswordFormVisible('false')
+    else setPasswordFormVisible('true')
+  }
+
+  const handleEmailForm = () => {
+    if (emailVisible === 'true') setEmailFormVisible('false')
+    else setEmailFormVisible('true')
+  }
+
   return (
-    <Container className="UserInfo">
-      <h1>Your information</h1>
-
-      <Paper>
-        <Grid container direction="row" alignItems="center" justify="center">
-          <Grid item xs={12}>
-            <List>
-              <ListItem>
-                <ListItemText
-                  primary="First name"
-                  secondary={props.firstName}
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemText
-                  primary="Last name"
-                  secondary={props.lastName}
-                />
-              </ListItem>
-            </List>
+    <Container>
+      <Container className="UserInfo">
+        <h1>Your information</h1>
+        <Paper>
+          <Grid container direction="row" alignItems="center" justify="center">
+            <Grid item xs={12}>
+              <List>
+                <ListItem>
+                  <ListItemText
+                    primary="First name"
+                    secondary={props.firstName}
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="Last name"
+                    secondary={props.lastName}
+                  />
+                </ListItem>
+                <ListItem>
+                  <ListItemText
+                    primary="Email"
+                    secondary={props.email}
+                  />
+                </ListItem>
+              </List>
+            </Grid>
           </Grid>
+        </Paper>
 
-          <Grid item xs={6}>
-            <List>
-              <ListItem>
-                <ListItemText
-                  primary="Email"
-                  secondary={props.email}
-                />
-              </ListItem>
-            </List>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Button style={{minWidth: '170px'}} variant="contained" component={Link} to="/changeemail" color="primary">
-              Change email
-        </Button>
-          </Grid>
-
-          <Grid item xs={6}>
-            <List>
-              <ListItem>
-                <ListItemText
-                  primary="Password"
-                  secondary={props.password.substring(0, 3) + "******"}
-                />
-              </ListItem>
-            </List>
-          </Grid>
-
-          <Grid item xs={6}>
-            <Button style={{minWidth: '170px'}} variant="contained" component={Link} to="/changepassword" color="primary">
-              Change password
-        </Button>
-          </Grid>
-        </Grid>
-      </Paper>
-
-      <Grid item xs={6}>
-        <List>
-          <ListItem>
-            <Button color="primary" onClick={handleClickOpen}>
-              Delete account
+        <Grid container direction="row" alignItems="center">
+          <List>
+            <ListItem>
+              <Button color="primary" onClick={handleClickOpen}>
+                Delete account
       </Button>
-            <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="alert-dialog-title"
-              aria-describedby="alert-dialog-description"
-            >
-              <DialogTitle id="alert-dialog-title">{"Are you sure you want to delete your account?"}</DialogTitle>
-              <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                  If you delete something will happen, maybe disadvantage you...
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">{"Are you sure you want to delete your account?"}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    If you delete something will happen, maybe disadvantage you...
           </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                  Cancel
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} color="primary">
+                    Cancel
           </Button>
-                <Button onClick={handleClose} component={Link} to="/login" color="primary" autoFocus>
-                  Continue
+                  <Button onClick={handleClose, () => { alert('A request to delete your account has been sent to the admins') }} component={Link} to="/login" color="primary" autoFocus>
+                    Continue
           </Button>
-              </DialogActions>
-            </Dialog>
-          </ListItem>
-        </List>
+                </DialogActions>
+              </Dialog>
+            </ListItem>
+          </List>
+
+          <Button onClick={handlePasswordForm} style={{ minWidth: '170px' }} color="primary">
+            Change password
+        </Button>
+
+          <Button onClick={handleEmailForm} style={{ minWidth: '170px' }} color="primary">
+            Change email
+        </Button>
+        </Grid>
+      </Container >
+
+      <Grid container>
+        <Grid item xs={6}>
+          {pwVisible === 'true' && (
+            <Container>
+              <h1>Change Password</h1>
+              <PasswordForm />
+            </Container>
+          )}
+        </Grid>
+        <Grid item xs={6}>
+          {emailVisible === 'true' && (
+            <Container>
+              <h1>Change Email</h1>
+              <EmailForm />
+            </Container>
+          )}
+        </Grid>
       </Grid>
 
     </Container >
