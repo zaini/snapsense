b84a0b5e9b87cd318a2e63b1db1ed7ca7880b3d6
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-22 02:39:22 +0000

    Add mutation to create relation between a patient and doctor

--- backend/graphql/resolvers/patients.js
+++ backend/graphql/resolvers/patients.js
@@ -1,5 +1,7 @@
-const { Patient } = require("../../models/index.js");
+const { ApolloError } = require("apollo-server");
 const argon2 = require("argon2");
+
+const { Doctor, Patient } = require("../../models/index.js");
 
 module.exports = {
   Query: {
@@ -23,6 +25,22 @@ module.exports = {
       }).save();
 
       return { ...patient.dataValues };
+    },
+    addPatientToDoctor: async (_, { patient_email, doctor_email }) => {
+      const doctor = await Doctor.findOne({ where: { email: doctor_email } });
+      const patient = await Patient.findOne({
+        where: { email: patient_email },
+      });
+
+      if (doctor) {
+        if (patient) {
+          await doctor.addPatient(patient);
+          await patient.addPatient(doctor);
+          return true;
+        }
+        throw new ApolloError("Invalid patient", 400);
+      }
+      throw new ApolloError("Invalid doctor!", 400);
     },
   },
 };
