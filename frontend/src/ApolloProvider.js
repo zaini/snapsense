cd3c37657cb075aa39f30a72614e11acc8e419e4
zaini <alimzaini@outlook.com>
2021-02-22 03:13:53 +0000

    Fix ApolloProvider and ReactDOM render

--- frontend/src/ApolloProvider.js
+++ frontend/src/ApolloProvider.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import App from './App';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { setContext } from 'apollo-link-context';	
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const backendLink = createHttpLink({
-    uri: process.env.BACKEND_URL || 'http://192.168.0.31:5000/graphql'
+  uri: process.env.BACKEND_URL || "http://192.168.0.31:5000/graphql",
 });
-
 
 // TODO: Once login is setup, use setContext to create an auth context
 
-
 const client = new ApolloClient({
-	link: backendLink,
-	cache: new InMemoryCache(),
-	connectToDevTools: true
+  link: backendLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
-
 export default () => {
-	return (
-		<ApolloProvider client={client}>
-			<App/>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
+  );
 };
