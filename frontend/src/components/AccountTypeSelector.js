90da1cb0d660be7eb7c70a3f38e7cf5cd40bdaf7
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-02-22 13:14:24 +0300

    Add type selection css and a new form to try out

--- frontend/src/components/AccountTypeSelector.js
+++ frontend/src/components/AccountTypeSelector.js
@@ -1,4 +1,5 @@
 import { Radio, RadioGroup, FormControlLabel } from "@material-ui/core";
+import './AccountTypeSelector.css';
 
 const LoginForm = ({ accountType, onAccountTypeChange }) => {
   return (
@@ -8,8 +9,47 @@ const LoginForm = ({ accountType, onAccountTypeChange }) => {
         control={<Radio />}
         label="🤒 Patient"
       />
-      <FormControlLabel value="doctor" control={<Radio />} label="👩‍⚕️ Doctor" />
+      <FormControlLabel   
+        value="doctor" 
+        control={<Radio />} 
+        label="👩‍⚕️ Doctor" />
+      
     </RadioGroup>
+
+    // <div class="main-container">
+    //   <div class="radio-buttons">
+    //     <label class="custom-radio">
+    //       <input type="radio" name="radio" />
+    //       <span class="radio-btn"
+    //         ><i class="las la-check"></i>
+    //         <div class="hobbies-icon">
+    //           <i class="las la-band-aid"></i>
+    //           <h3>Patient</h3>
+    //         </div>
+    //       </span>
+    //     </label>
+    //     <label class="custom-radio">
+    //       <input type="radio" name="radio" />
+    //       <span class="radio-btn"
+    //         ><i class="las la-check"></i>
+    //         <div class="hobbies-icon">
+    //          <i class="las la-stethoscope"></i>
+    //           <h3>Doctor</h3>
+    //         </div>
+    //       </span>
+    //     </label>
+    //     <label class="custom-radio">
+    //       <input type="radio" name="radio" />
+    //       <span class="radio-btn"
+    //         ><i class="las la-check"></i>
+    //         <div class="hobbies-icon">
+    //           <i class="las la-hospital-symbol"></i>
+    //           <h3>Admin</h3>
+    //         </div>
+    //       </span>
+    //     </label>
+    //   </div>
+    // </div>
   );
 };
 
