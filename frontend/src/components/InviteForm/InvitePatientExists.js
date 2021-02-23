7a3f1dd5c18967284abc05e5f28a692e2842d9e0
zaini <alimzaini@outlook.com>
2021-02-23 18:55:08 +0000

    Add login btn if patient not logged in at invite

--- frontend/src/components/InviteForm/InvitePatientExists.js
+++ frontend/src/components/InviteForm/InvitePatientExists.js
@@ -1,23 +1,50 @@
-import React from "react";
+import { useContext } from "react";
 import { Box, Button, Center } from "@chakra-ui/react";
+import { AuthContext } from "../../context/auth";
+import { useHistory } from "react-router-dom";
 
 const InvitePatientExists = ({ invitation }) => {
+  const history = useHistory();
+  const { user } = useContext(AuthContext);
+
   return (
     <Box p="7" borderWidth="1px" borderRadius="lg">
       <h1>
         You've been invited by {invitation.inviterEmail} to join their clinic.
-        You already have an account, so would you like to be added to their
-        clinic?
+        You already have an account so you will be added to this clinician's
+        patients list.
       </h1>
       <br />
-      <Center>
-        <Button mt={4} mr={4} colorScheme="blue" type="submit">
-          Accept Invite
-        </Button>
-        <Button mt={4} colorScheme="red" type="submit">
-          Decline Invite
-        </Button>
-      </Center>
+      <Box
+        container
+        justify="center"
+        direction="column"
+        alignItems="center"
+        textAlign="center"
+      >
+        {user && user.accountType === "PATIENT" ? (
+          <>
+            <Button mt={4} mr={4} colorScheme="blue" type="submit">
+              Accept Invite
+            </Button>
+            <Button mt={4} colorScheme="red" type="submit">
+              Decline Invite
+            </Button>
+          </>
+        ) : (
+          <>
+            <p>Please login to accept this invitation.</p>
+            <Button
+              mt={4}
+              mr={4}
+              colorScheme="blue"
+              onClick={() => history.push("/login")}
+            >
+              Login
+            </Button>
+          </>
+        )}
+      </Box>
     </Box>
   );
 };
