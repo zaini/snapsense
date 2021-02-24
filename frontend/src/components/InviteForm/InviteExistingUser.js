adb2135aa2318d1f7462f0d99aea09cf8af1dbeb
zaini <alimzaini@outlook.com>
2021-02-24 17:26:39 +0000

    Add render for login if not logged in for invite

--- frontend/src/components/InviteForm/InviteExistingUser.js
+++ frontend/src/components/InviteForm/InviteExistingUser.js
@@ -2,51 +2,47 @@ import { useContext } from "react";
 import { Box, Button, Center } from "@chakra-ui/react";
 import { AuthContext } from "../../context/auth";
 import { useHistory } from "react-router-dom";
+import LoginPage from "../../pages/LoginPage";
 
 const InvitePatientExists = ({ invitation }) => {
   const history = useHistory();
-  const { user } = useContext(AuthContext);
+  const { user, logout } = useContext(AuthContext);
 
-  return (
-    <Box p="7" borderWidth="1px" borderRadius="lg">
-      <h1>
-        You've been invited by {invitation.inviterEmail} to join their clinic.
-        You already have an account so you will be added to this clinician's
-        patients list.
-      </h1>
-      <br />
-      <Box
-        container
-        justify="center"
-        direction="column"
-        alignItems="center"
-        textAlign="center"
-      >
-        {user && user.accountType === "PATIENT" ? (
-          <>
-            <Button mt={4} mr={4} colorScheme="blue" type="submit">
-              Accept Invite
-            </Button>
-            <Button mt={4} colorScheme="red" type="submit">
-              Decline Invite
-            </Button>
-          </>
-        ) : (
-          <>
-            <p>Please login to accept this invitation.</p>
-            <Button
-              mt={4}
-              mr={4}
-              colorScheme="blue"
-              onClick={() => history.push("/login")}
-            >
-              Login
-            </Button>
-          </>
-        )}
+  if (
+    user &&
+    user.accountType === "PATIENT" &&
+    user.email === invitation.newAcccountEmail
+  ) {
+    return (
+      <Box p="7" borderWidth="1px" borderRadius="lg">
+        <h1>
+          You've been invited by {invitation.inviterEmail} to join their clinic.
+          You already have an account so you will be added to this clinician's
+          patients list if you accept.
+        </h1>
+        <br />
+        <Box
+          container
+          justify="center"
+          direction="column"
+          alignItems="center"
+          textAlign="center"
+        >
+          <Button mt={4} mr={4} colorScheme="blue" type="submit">
+            Accept Invite
+          </Button>
+          <Button mt={4} colorScheme="red" type="submit">
+            Decline Invite
+          </Button>
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  } else {
+    if (user) {
+      logout();
+    }
+    return <LoginPage />;
+  }
 };
 
 export default InvitePatientExists;
