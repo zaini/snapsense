30c2df617b0290fffeea1d62295becb733eb1d1e
zaini <alimzaini@outlook.com>
2021-02-24 17:59:46 +0000

    Update user from auth context

--- frontend/src/context/auth.js
+++ frontend/src/context/auth.js
@@ -5,14 +5,17 @@ const initialState = {
   user: null,
 };
 
-if (localStorage.getItem("jwtToken")) {
-  const decodedToken = decode(localStorage.getItem("jwtToken"));
-  if (decodedToken && decodedToken.exp * 1000 < Date.now()) {
-    localStorage.removeItem("jwtToken");
-  } else {
-    initialState.user = decodedToken;
+const updateUser = () => {
+  if (localStorage.getItem("jwtToken")) {
+    const decodedToken = decode(localStorage.getItem("jwtToken"));
+    if (decodedToken && decodedToken.exp * 1000 < Date.now()) {
+      localStorage.removeItem("jwtToken");
+    } else {
+      initialState.user = decodedToken;
+    }
   }
-}
+  return initialState.user;
+};
 
 const AuthContext = createContext({
   user: null,
@@ -43,21 +46,21 @@ const AuthProvider = (props) => {
   const login = (userData) => {
     localStorage.setItem("jwtToken", userData.accessToken);
     dispatch({
-        type: "LOGIN",
-        payload: userData,
+      type: "LOGIN",
+      payload: userData,
     });
   };
 
   const logout = () => {
     localStorage.removeItem("jwtToken");
     dispatch({
-        type: "LOGOUT",
+      type: "LOGOUT",
     });
   };
 
   return (
     <AuthContext.Provider
-      value={{ user: state.user, login, logout }}
+      value={{ user: updateUser(), login, logout }}
       {...props}
     />
   );
