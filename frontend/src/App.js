f77775481f851ed9bb1776efde6f3cfd5a541af7
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-02 00:49:51 +0400

    Import Landing Page
    Add Landing Page to List of imports
    Comment out existing Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage.js";
+import LandingPage from "./pages/LandingPage.js";
 import LoginPage from "./pages/LoginPage.js";
 import LogoutPage from "./pages/LogoutPage.js";
 import Navbar from "./components/navBar/Navbar.js";
@@ -9,9 +10,10 @@ import CreateInvitePage from "./pages/CreateInvitePage.js";
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/landing" component={LandingPage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/invite/:token_id" component={InvitePage} />
         <Route exact path="/invite" component={CreateInvitePage} />
