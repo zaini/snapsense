9493d24dd86a57f1531948bbf1a07997ce64fc2c
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-02 19:43:44 +0400

    Refactor and Add components to Header.js
    -Add Dashbord link to Landing Page header.js
    -Refactor Menu Item component

--- frontend/src/components/sections/Header.js
+++ frontend/src/components/sections/Header.js
@@ -1,24 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Box, Flex, Text, Button } from "@chakra-ui/react";
+import { Box, Flex, Button } from "@chakra-ui/react";
 import Logo from "../ui/Logo";
 import { CloseIcon, MenuIcon } from "../ui/Icons";
 
-const MenuItem = ({ children, isLast, to = "/", ...rest }) => {
-  return (
-    <Text
-      mb={{ base: isLast ? 0 : 8, sm: 0 }}
-      mr={{ base: 0, sm: isLast ? 0 : 8 }}
-      display="block"
-      {...rest}
-    >
-      <Link to={to}>{children}</Link>
-    </Text>
-  );
-};
+import MenuItem from "../ui/MenuItem";
 
 const Header = (props) => {
-  console.log(props);
   const [show, setShow] = React.useState(false);
   const toggleMenu = () => setShow(!show);
 
@@ -31,9 +18,7 @@ const Header = (props) => {
       w="100%"
       mb={8}
       p={8}
-      bg={["primary.500", "primary.500", "transparent", "transparent"]}
-      color={["white", "white", "primary.700", "primary.700"]}
-      {...props}
+      color="primary.900"
     >
       <Flex align="center">
         <Logo
@@ -56,7 +41,7 @@ const Header = (props) => {
           direction={["column", "row", "row", "row"]}
           pt={[4, 4, 0, 0]}
         >
-          <MenuItem to="/">Home</MenuItem>
+          <MenuItem to="/dashboard">Home</MenuItem>
           <MenuItem to="/invite">Invite </MenuItem>
           <MenuItem to="/profile">Profile </MenuItem>
           <MenuItem to="/login" isLast>
