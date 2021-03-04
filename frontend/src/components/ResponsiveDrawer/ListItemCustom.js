b0b13384c7d4a397a318af96d275730aa665c21f
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-04 23:32:06 +0400

    Add custom List Component
    -Refactored code for Scaffold common Panel

---
+++ frontend/src/components/ResponsiveDrawer/ListItemCustom.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "@material-ui/core";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+
+const ListItemCustom = (props) => {
+  return (
+    <Link style={{textDecoration:"none"}}  href={props.link}>
+      <ListItem button key={props.text}>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary={props.text} />
+      </ListItem>
+    </Link>
+  );
+};
+
+export default ListItemCustom;
