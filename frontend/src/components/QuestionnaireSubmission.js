96eddddcae2e0303dcfae99fbb6553c6286cd172
Soyoung Park <p.soyoung63@gmail.com>
2021-03-04 23:36:42 +0900

    Add and set up image and questionnaire submissions from patients

---
+++ frontend/src/components/QuestionnaireSubmission.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Center } from "@chakra-ui/react"
+
+// import questionnaireData from "./file.json";
+// will need to import questionnaire data
+
+const LoginFormWrapper = () => {
+  return (
+    <Center w="100px">
+
+    </Center>
+  );
+};
+
+export default LoginFormWrapper;
+
