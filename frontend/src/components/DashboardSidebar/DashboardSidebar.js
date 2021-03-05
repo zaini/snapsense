86ea14eb6f7d73cdd8a8e024189043108c6582be
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-05 18:29:49 +0400

    Fix Header Hamburger Icon
    -Fix Layout issue for smaller screens

--- frontend/src/components/DashboardSidebar/DashboardSidebar.js
+++ frontend/src/components/DashboardSidebar/DashboardSidebar.js
@@ -10,6 +10,8 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import MenuIcon from "@material-ui/icons/Menu";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { InfoIcon, PhoneIcon, PlusSquareIcon } from "@chakra-ui/icons";
 import { Flex } from "@chakra-ui/react";
@@ -75,7 +77,23 @@ const DashboardSidebar = (props) => {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <IconButton
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar back style={{ background: "#9C3848" }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            {props.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* <IconButton
         color="inherit"
         aria-label="open drawer"
         edge="start"
@@ -83,14 +101,13 @@ const DashboardSidebar = (props) => {
         className={classes.menuButton}
       >
         <MenuIcon />
-      </IconButton>
+      </IconButton> */}
       <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
           <Drawer
             container={container}
             variant="temporary"
-            anchor={theme.direction === "rtl" ? "right" : "left"}
+            anchor={"left"}
             open={mobileOpen}
             onClose={handleDrawerToggle}
             classes={{
