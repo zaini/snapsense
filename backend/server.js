be328ec34d8da8cae4954064851248fc49368ac5
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-05 15:13:00 +0000

    Fix code formatting

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
-const app = require('./index')
+const app = require("./index");
 const port = process.env.PORT || 5000;
-
 
 app.listen(port, () => {
   console.log(`Server ready at http://localhost:${port}/`);
