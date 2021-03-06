9f4f53d30d03825c717ca1f779f54f528be90945
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-06 18:02:14 +0400

    Add toolbar to DataGrid component
    -Add toolbars
    -Make Responsive using Flex Box

--- frontend/src/components/incomplete/Table.js
+++ frontend/src/components/incomplete/Table.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { DataGrid } from "@material-ui/data-grid";
 import { Box } from "@material-ui/core";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridColumnsToolbarButton,
+  GridFilterToolbarButton,
+} from "@material-ui/data-grid";
+
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridColumnsToolbarButton />
+      <GridFilterToolbarButton />
+    </GridToolbarContainer>
+  );
+}
 
 // Component which shows the patients history of uploads
 //TODO: Update the link to the page which displays the details of the upload
-const Table = ({ data, cols }) => {
-  return (
-    <>
-      <Box height="73vh" width="100%">
-        <DataGrid columns={cols} rows={data} autoPageSize />
-      </Box>
-    </>
-  );
-};
 
-export default Table;
+export default function Table({ data, cols }) {
+  return (
+    <div style={{ height: 600, width: "100%" }}>
+      <DataGrid
+        columns={cols}
+        rows={data}
+        components={{
+          Toolbar: CustomToolbar,
+        }}
+      />
+    </div>
+  );
+}
