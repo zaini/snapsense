6f87aa90198a6fdad835bdce2f905536cfddc71d
zaini <alimzaini@outlook.com>
2021-03-10 22:18:47 +0000

    Refactor getSubmissions resolver

--- backend/graphql/resolvers/submissions.js
+++ backend/graphql/resolvers/submissions.js
@@ -1,14 +1,47 @@
 const { Submission } = require("../../models/index.js");
+const isAuth = require("../../utils/isAuth");
 
 module.exports = {
   Query: {
-    getSubmissions: async () => {
-      try {
-        const submissions = await Submission.findAll();
-        return submissions;
-      } catch (error) {
-        throw new Error(error);
+    getSubmissions: async (_, { patient_id: id }, context) => {
+      const user = isAuth(context);
+
+      const account_type = user.account_type;
+      let submissions = [];
+
+      // TODO check this actually works lol
+
+      // If user is patient, just return all their submissions
+      // If user is doctor,
+      // If doctor asked for patient_id: return all the submissions for the correct patient based on patient_id
+      // If doctor did not provide a patient_id: return all submissions from all their patients
+      // Else return error
+      switch (account_type) {
+        case "DOCTOR":
+          const doctor = await Doctor.findByPk(user.id);
+
+          if (id) {
+            const patient = await Patient.findByPk(id);
+            if (await doctor.hasPatient(patient)) {
+              submissions = await patient.getSubmissions();
+            }
+          } else {
+            const patients = await doctor.getPatients();
+            patients.forEach(async (patient) => {
+              const patientSubmissions = await patient.getSubmissions();
+              submissions.push(patientSubmissions);
+            });
+          }
+
+          return submissions;
+        case "PATIENT":
+          const patient = await Patient.findByPk(user.id);
+          submissions = await patient.getSubmissions();
+        default:
+          break;
       }
+
+      return submissions;
     },
   },
   Mutation: {
