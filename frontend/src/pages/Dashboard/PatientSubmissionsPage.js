ea50af17be3dbaae579b55d1ae2bce03aa49a28d
zaini <alimzaini@outlook.com>
2021-03-10 21:31:58 +0000

    Add comment for patient submissions table

--- frontend/src/pages/Dashboard/PatientSubmissionsPage.js
+++ frontend/src/pages/Dashboard/PatientSubmissionsPage.js
@@ -1,6 +1,9 @@
 import PatientSubmissionsTable from "../../components/PatientSubmissionsTable";
 
 const PatientSubmissionsPage = () => {
+  // TODO you'd pass data to the table from here, so it can show stuff dynamically based on which data you have
+  // should work for both patients and doctors.
+  // I think this table should be the time that Era is working on with the timelines/table and toggle option.
   return <PatientSubmissionsTable />;
 };
 
