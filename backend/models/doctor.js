66886fc886b9d245dc58032aa516c716980eefc0
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-10 03:10:52 +0000

    Refactor doctor model to use new request model

--- backend/models/doctor.js
+++ backend/models/doctor.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
         through: "Doctor_Patient_Relation",
         foreignKey: "doctor_id",
       });
-      Doctor.hasMany(models.Submission, { foreignKey: "doctor_id" });
+      Doctor.hasMany(models.Request, { foreignKey: "doctor_id" });
     }
   }
   Doctor.init(
