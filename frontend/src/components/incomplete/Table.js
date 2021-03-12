c4d8bd6a0eb126b5f57a0bf9333f524695acce1c
zaini <alimzaini@outlook.com>
2021-03-12 21:31:54 +0000

    Add My Doctors table w/ mock data

--- frontend/src/components/incomplete/Table.js
+++ frontend/src/components/incomplete/Table.js
@@ -6,8 +6,8 @@ import { Box } from "@chakra-ui/react";
 // TODO: Update the link to the page which displays the details of the upload
 const Table = ({ data, cols }) => {
   return (
-    <Box height="73vh" width="100%">
-      <DataGrid columns={cols} rows={data} autoPageSize />
+    <Box height="73vh">
+      <DataGrid columns={cols} rows={data} />
     </Box>
   );
 };
--- frontend/src/pages/My/DoctorsPage.js
+++ frontend/src/pages/My/DoctorsPage.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import { Heading } from "@chakra-ui/layout";
+import React from "react";
+import Table from "../../components/incomplete/Table";
+import { Container } from "@material-ui/core";
 
 // If admin: shows all doctors from the same hospital
 // If patient: shows all doctors that you have
 const DoctorsPage = () => {
+  const cols = [
+    {
+      field: "id",
+      type: "number",
+      headerName: "ID",
+      sortable: true,
+      flex: 0.3,
+    },
+    {
+      field: "fname",
+      headerName: "First Name",
+      sortable: true,
+      flex: 1,
+    },
+    {
+      field: "lname",
+      headerName: "Last Name",
+      sortable: true,
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      sortable: true,
+      flex: 2,
+    },
+  ];
+
+  const data = [
+    { id: 2, fname: "firstname", lname: "lastname", email: "email@email.com" },
+  ];
+
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Heading>My Doctors</Heading>
+      <br />
+      <Table data={data} cols={cols} />
+    </Container>
   );
 };
 
