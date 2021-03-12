3eaea14b5a158e0176b66eebdb03801ac367d284
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-03-12 01:28:44 +0000

    Add materiul-lab package for timeline

--- frontend/src/components/PatientsPersonalLogTimeline.js
+++ frontend/src/components/PatientsPersonalLogTimeline.js
@@ -1,4 +1,18 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Timeline from '@material-ui/lab/Timeline';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import LaptopMacIcon from '@material-ui/icons/LaptopMac';
+import HotelIcon from '@material-ui/icons/Hotel';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const PatientsPersonalLogTimeline = () => {
   return <div></div>;
