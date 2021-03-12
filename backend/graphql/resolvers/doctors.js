049e671f0bbfaf44dd6526aded9f332b862689a8
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-12 23:12:06 +0000

    Add backend functionality to retrieve a patient by a doctor

--- backend/graphql/resolvers/doctors.js
+++ backend/graphql/resolvers/doctors.js
@@ -1,4 +1,5 @@
-const { Doctor, Hospital } = require("../../models/index.js");
+const { AuthenticationError, UserInputError } = require("apollo-server-core");
+const { Patient, Doctor, Hospital } = require("../../models/index.js");
 const isAuth = require("../../utils/isAuth");
 
 module.exports = {
@@ -16,22 +17,57 @@ module.exports = {
     getDoctorsByHospital: async (_, __, context) => {
       const user = isAuth(context);
 
-      if (!(user.accountType === "ADMIN")) {
+      // Allow only admins
+      if (user.accountType !== "ADMIN") {
         throw new AuthenticationError(
           "You are not logged into the correct account for this feature."
         );
       }
 
-      const { hospital_id } = user;
+      // Get the hospital and check it exists
+      const hospital = await Hospital.findByPk(user.hospital_id);
 
-      try {
-        const doctors = await Doctor.findAll({
-          where: { hospital_id: hospital_id },
-        });
-        return doctors;
-      } catch (error) {
-        throw new Error(error);
+      if(!hospital) {
+        throw new UserInputError("Invalid hospital")
       }
+
+      const doctors = await hospital.getDoctors();
+      return doctors || []; // doctors might be null, so return empty array
+    },
+    getPatientByDoctor: async (_, { patient_id }, context) => {
+      const user = isAuth(context);
+
+      // Authenticate doctor
+      if (!user.accountType || user.accountType !== "DOCTOR") {
+        throw new AuthenticationError("Invalid user credentials!");
+      }
+
+      // Get the doctor and make sure they exist
+      const doctor = await Doctor.findByPk(user.id);
+      if (!doctor) {
+        throw new UserInputError("Invalid user!");
+      }
+      
+      // Get the patient and make sure they exist
+      const patient = await Patient.findByPk(patient_id);
+      if (!patient) {
+        throw new UserInputError("Invalid patient!");
+      }
+
+      // If the patient belongs to the doctor, return the patient
+      if(await doctor.hasPatient(patient)) {
+        return {
+          id: patient.id,
+          fname: patient.fname,
+          lname: patient.lname,
+          email: patient.email,
+          createdAt: patient.createdAt,
+
+        };
+      }
+      
+      // Patient does not belong to doctor, throw error
+      throw new UserInputError("Invalid patient!");
     },
   },
   Mutation: {},
