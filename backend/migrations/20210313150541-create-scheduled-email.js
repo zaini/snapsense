a03be3e3c89e0652b20e27d5b535304a09eb788a
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-13 20:19:13 +0400

    Add email scheduler model

---
+++ backend/migrations/20210313150541-create-scheduled-email.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ScheduledEmails', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      to: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      subject: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      html: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      altbody: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      template: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ScheduledEmails');
+  }
+};
