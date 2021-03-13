a945e0a1da43a814341aea4ecd7aca3480a7c89c
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-13 00:33:21 +0000

    Format code

--- backend/graphql/resolvers/request.js
+++ backend/graphql/resolvers/request.js
@@ -11,14 +11,13 @@ module.exports = {
       { request_type, deadline, patient_id },
       context
     ) => {
-
-			// Authenticate user
+      // Authenticate user
       const user = isAuth(context);
       if (!user.accountType || user.accountType !== "DOCTOR") {
         throw new AuthenticationError("Invalid user credentials!");
       }
 
-			// Check that the doctor and patient both exist
+      // Check that the doctor and patient both exist
       const doctor = await Doctor.findByPk(user.id);
       if (!doctor) {
         throw new UserInputError("Invalid user!");
@@ -29,30 +28,34 @@ module.exports = {
         throw new UserInputError("Invalid patient!");
       }
 
-			// Check that the doctor 'has' this patient
-			if(! await doctor.hasPatient(patient)) {
-				throw new UserInputError("This patient does not belong to you!!");
-			}
+      // Check that the doctor 'has' this patient
+      if (!(await doctor.hasPatient(patient))) {
+        throw new UserInputError("This patient does not belong to you!!");
+      }
 
       // Source for date format below: https://stackoverflow.com/questions/8362952/javascript-output-current-datetime-in-yyyy-mm-dd-hhmsec-format
       try {
-				// Create the request and save it
+        // Create the request and save it
         const request = await new Request({
           doctor_id: doctor.id,
           patient_id: patient.id,
           type: request_type,
-          deadline: (new Date(parseInt(deadline))).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " "), 
+          deadline: new Date(parseInt(deadline))
+            .toISOString()
+            .slice(0, 19)
+            .replace(/-/g, "/")
+            .replace("T", " "),
         }).save();
 
-				// Assign the request to both the doctor and patient
+        // Assign the request to both the doctor and patient
         doctor.addRequest(request);
         patient.addRequest(request);
       } catch (error) {
-				// An error will be thrown if the request is invalid as a result of a user input error
+        // An error will be thrown if the request is invalid as a result of a user input error
         throw new UserInputError(error);
       }
 
-			// Everything was successful so return false
+      // Everything was successful so return false
       return true;
     },
   },
