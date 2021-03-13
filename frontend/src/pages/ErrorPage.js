59995edec73671f4c90e699e28bec1df940a6b18
zaini <alimzaini@outlook.com>
2021-03-13 17:43:09 +0000

    Add error page

---
+++ frontend/src/pages/ErrorPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <Alert
+        status="error"
+        variant="subtle"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        height="100vh"
+      >
+        <AlertIcon boxSize="40px" mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize="lg">
+          An Error Occured!
+        </AlertTitle>
+        <AlertDescription maxWidth="sm">
+          Something went wrong. If this persists, please contact us{" "}
+          {<Link to="/contact">here.</Link>}
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
+};
+
+export default ErrorPage;
