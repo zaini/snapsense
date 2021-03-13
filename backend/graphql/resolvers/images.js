74f463bc039f157923662b9ae2bbad42bc95021b
Era Mullahasani Dula <era.mullahasani_dula@kcl.ac.uk>
2021-03-13 19:55:56 +0000

    Add image query

--- backend/graphql/resolvers/images.js
+++ backend/graphql/resolvers/images.js
@@ -1,6 +1,21 @@
 const imageUploader = require("../../utils/filestreamUploader");
 
 module.exports = {
+  Query:{
+    getImages: async () => {
+      try {
+        const images = await Images.findAll();
+        return images;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    getImagesBySubmission: async (_, { submission_id: id }, __) => {
+      const submission = await Submission.findByPk(id);
+      const images = submission.getImages();
+      return images || []
+    }
+  },
   Mutation: {
     singleUploadStream: async (parent, { file }) => {
       const response = await imageUploader(file);
