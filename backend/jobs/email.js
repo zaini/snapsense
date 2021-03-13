df4f240d260d218a4a1481e044ece80bc4fba361
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-14 01:10:22 +0400

    Add template support to email.js

--- backend/jobs/email.js
+++ backend/jobs/email.js
@@ -1,92 +1,77 @@
 const os = require("os");
-const Email = require("email-templates");
 const pMap = require("p-map");
 const { parentPort } = require("worker_threads");
-const transporter = require("../utils/mailTransporter");
-
-const email = new Email({
-  message: {
-    from: "seg.snapsense.project@gmail.com",
-  },
-  transport: {
-    jsonTransport: true,
-  },
-});
-
-email.transport = transporter;
-
-// store boolean if the job is cancelled
-let isCancelled = false;
-
-// how many emails to send at once
-const concurrency = os.cpus().length;
-
-console.log("Concurrency: " + concurrency);
-// example database results
-const results = [
-  {
-    template: 'welcome',
-    message: {
-      to: "ayanahmad.ahay@gmail.com",
-    },
-    locals: {
-      name: "Ayan1 Ahmad",
-    },
-  },
-  // {
-  //   template: "welcome",
-  //   message: {
-  //     to: "ayanahmad5701@gmail.com",
-  //   },
-  //   locals: {
-  //     name: "Ayan2 Ahmad",
-  //   },
-  // },
-  // {
-  //   template: "welcome",
-  //   message: {
-  //     to: "anonymous.code.anonymous@gmail.com",
-  //   },
-  //   locals: {
-  //     name: "Ayan3 Ahmad",
-  //   },
-  // },
-];
-console.log("Almost ready to send email");
-
-async function mapper(result) {
-  // return early if the job was already cancelled
-  if (isCancelled) return;
-  try {
-    const response = await email.send(result);
-    console.log("sent email");
-    // here is where you would write to the database that it was sent
-    return response;
-  } catch (err) {
-    // catch the error so if one email fails they all don't fail
-    console.log(err);
-  }
-}
-
-console.log("Declared Mapper Function");
-
-// handle cancellation (this is a very simple example)
-if (parentPort)
-  parentPort.once("message", (message) => {
-    //
-    // TODO: once we can manipulate concurrency option to p-map
-    // we could make it `Number.MAX_VALUE` here to speed cancellation up
-    // <https://github.com/sindresorhus/p-map/issues/28>
-    //
-    if (message === "cancel") isCancelled = true;
-  });
+const sendMail = require("../utils/mailSender");
+const { ScheduledEmail } = require("../models/index.js");
+const giveTemplate = require("../utils/emailTemplateProvider");
 
 (async () => {
-  // query database results for emails not sent
-  // and iterate over them with concurrency
-  await pMap(results, mapper, { concurrency });
+  let isCancelled = false;
+  const concurrency = os.cpus().length;
+  let sendingIds = [];
+
+  async function emailStatusChanger(newStatus, id) {
+    await ScheduledEmail.update(
+      { status: newStatus },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+  }
+
+  async function mapper(result) {
+    // return early if the job was already cancelled
+    if (isCancelled) return;
+    try {
+      const htmlContent = giveTemplate(result.template,result.html); 
+      // console.log(htmlContent);
+      const response = await sendMail(
+        result.to,
+        result.subject,
+        htmlContent,
+        result.altbody
+      );
+      await emailStatusChanger(3, result.id);
+      return response;
+    } catch (err) {
+      await emailStatusChanger(2, result.id);
+      console.log(err);
+    }
+  }
+
+  // status:
+  // 0 new
+  // 1 pending
+  // 2 error sending
+  // 3 sent
+
+  const emailsFetched = await ScheduledEmail.findAll({
+    where: { status: 0 },
+    limit: 25,
+  });
+
+  if (emailsFetched.length > 0) {
+    emailsFetched.map((e, i) => {
+      sendingIds.push(e.dataValues.id);
+    });
+    await emailStatusChanger(1, sendingIds);
+
+    if (parentPort) {
+      parentPort.once("message", (message) => {
+        if (message === "cancel") isCancelled = true;
+      });
+    }
+
+    await pMap(emailsFetched, mapper, { concurrency });
+  } else {
+    console.log("___________No Emails___________");
+  }
 
   // signal to parent that the job is done
   if (parentPort) parentPort.postMessage("done");
   else process.exit(0);
+
+
 })();
