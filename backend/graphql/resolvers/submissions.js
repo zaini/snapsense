c80d6556fe66a2b3578b3fa1162b6e9a746b061d
zaini <alimzaini@outlook.com>
2021-03-14 00:00:36 +0000

    Remove get specific submissions

--- backend/graphql/resolvers/submissions.js
+++ backend/graphql/resolvers/submissions.js
@@ -43,14 +43,6 @@ module.exports = {
 
       return submissions || [];
     },
-
-    //getSubmissionType
-
-    getSpecificSubmission: async (_, { submission_id: id }, __) => {
-      //account for such submission not existing? null case
-      const submission = await Submission.findByPk(id);
-      return submission;
-    },
   },
   Mutation: {
     createSubmission: async (_, submission_details) => {
