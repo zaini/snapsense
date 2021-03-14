114ea9495e0fd06ae20f93a96aa245cf49a060bc
mahsumkocabey <mahsumkocabey@hotmail.com>
2021-03-14 17:51:39 +0000

    Centered items

--- frontend/src/pages/My/NewSubmissionPage.js
+++ frontend/src/pages/My/NewSubmissionPage.js
@@ -5,7 +5,17 @@ import { SimpleGrid } from "@chakra-ui/react";
 // Create a new submission as a patient
 const NewSubmissionPage = () => {
   return (
-    <SimpleGrid columns={[1, 1, 1, 2]} spacing={2} w={"100%"}>
+    <SimpleGrid
+      columns={[1, 1, 1, 1]}
+      spacing={2}
+      w={"100%"}
+      p="30"
+      borderWidth="2em"
+      borderRadius="lg"
+      marginLeft="auto"
+      marginRight="auto"
+      mt="30"
+    >
       <ImageUpload />
       <Questionnaire />
     </SimpleGrid>
