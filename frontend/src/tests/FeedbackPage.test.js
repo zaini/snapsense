973c42ef805819d1e9a126ad574e351041ffb2db
Sonia Baileys@less ~/.shh/id_rsa.publess ~/shh/id_rsa.pubshh-keygen -t rsa -b 4096 -C sonia.baileys@kcl.ac.ukgit config --global user.name Sonia <sonia.baileys@kcl.ac.uk>
2021-03-16 15:01:52 +0300

    Set up frontend pages for tests

---
+++ frontend/src/tests/FeedbackPage.test.js
@@ -0,0 +1,2 @@
+import { render, screen } from "@testing-library/react";
+import FeedbackPage from "../FeedbackPage";
---
+++ frontend/src/tests/LandingPage.test.js
@@ -0,0 +1,2 @@
+import { render, screen } from "@testing-library/react";
+import LandingPage from "../LandingPage";
