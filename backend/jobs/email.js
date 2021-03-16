284b5146b05bbfd936bc48f4002530c4df9ed562
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-16 15:42:01 +0400

    Refactor Email.js

--- backend/jobs/email.js
+++ backend/jobs/email.js
@@ -1,6 +1,7 @@
 const os = require("os");
 const pMap = require("p-map");
 const { parentPort } = require("worker_threads");
+
 const sendMail = require("../utils/mailSender");
 const { ScheduledEmail } = require("../models/index.js");
 const giveTemplate = require("../utils/emailTemplateProvider");
@@ -11,50 +12,50 @@ const giveTemplate = require("../utils/emailTemplateProvider");
 // 2 error sending
 // 3 sent
 
+// this is responsible for setting the status of an/various id(s) belonging to the scheduledEmails model
+const emailStatusChanger = async (newStatus, id) => {
+  await ScheduledEmail.update(
+    { status: newStatus },
+    {
+      where: {
+        id: id,
+      },
+    }
+  );
+};
+
+// each ScheduledEmail object retrived will be passed through this
+const mapper = async (result) => {
+  // return early if the job was already cancelled
+  if (isCancelled) return;
+  try {
+    // Retrieve HTML code by passing essential information to Template Provider
+    const htmlContent = giveTemplate(result.template, result.html);
+
+    // Send main using Nodemailer
+    const response = await sendMail(
+      result.to,
+      result.subject,
+      htmlContent,
+      result.altbody
+    );
+
+    // If the email was succesfully sent, set the ID for this ScheduledEmail to 3
+    await emailStatusChanger(3, result.id);
+
+    return response;
+  } catch (err) {
+    // If the email was not succesfully sent, set the ID for this ScheduledEmail to 2
+    await emailStatusChanger(2, result.id);
+    console.log(err);
+  }
+};
+
 // Calling an immediately invoked function in order to enable async/await functionality
 (async () => {
   let isCancelled = false;
   const concurrency = os.cpus().length;
   let sendingIds = [];
-
-  // this is responsible for setting the status of an/various id(s) belonging to the scheduledEmails model
-  async function emailStatusChanger(newStatus, id) {
-    await ScheduledEmail.update(
-      { status: newStatus },
-      {
-        where: {
-          id: id,
-        },
-      }
-    );
-  }
-
-  // each ScheduledEmail object retrived will be passed through this
-  async function mapper(result) {
-    // return early if the job was already cancelled
-    if (isCancelled) return;
-    try {
-      // Retrieve HTML code by passing essential information to Template Provider
-      const htmlContent = giveTemplate(result.template, result.html);
-
-      // Send main using Nodemailer
-      const response = await sendMail(
-        result.to,
-        result.subject,
-        htmlContent,
-        result.altbody
-      );
-
-      // If the email was succesfully sent, set the ID for this ScheduledEmail to 3
-      await emailStatusChanger(3, result.id);
-
-      return response;
-    } catch (err) {
-      // If the email was not succesfully sent, set the ID for this ScheduledEmail to 2
-      await emailStatusChanger(2, result.id);
-      console.log(err);
-    }
-  }
 
   // Fetch all scheduled emails that have not been processed yet, id=0
   const emailsFetched = await ScheduledEmail.findAll({
