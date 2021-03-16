eb9e5685961395f07f6c3e9c0d37e6bc2ca502b8
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-16 21:30:15 +0000

    Hide bree cron job

--- backend/index.js
+++ backend/index.js
@@ -8,20 +8,20 @@ require("dotenv").config();
 const typeDefs = require("./graphql/typeDefs/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
-const bree = new Bree({
-  jobs: [
-    {
-      name: "email",
-      interval: "25s",
-    },
-  ],
-});
+// const bree = new Bree({
+//   jobs: [
+//     {
+//       name: "email",
+//       interval: "25s",
+//     },
+//   ],
+// });
 
-// handle graceful reloads, pm2 support, and events like SIGHUP, SIGINT, etc.
-const graceful = new Graceful({ brees: [bree] });
-graceful.listen();
-// start all jobs (this is the equivalent of reloading a crontab):
-bree.start();
+// // handle graceful reloads, pm2 support, and events like SIGHUP, SIGINT, etc.
+// const graceful = new Graceful({ brees: [bree] });
+// graceful.listen();
+// // start all jobs (this is the equivalent of reloading a crontab):
+// bree.start();
 
 const app = express();
 
