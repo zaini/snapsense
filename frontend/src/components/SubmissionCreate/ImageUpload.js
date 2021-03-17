7b34e59ca22f0a81504599e138147d5d266e668b
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-17 17:25:21 +0400

    Add clean image-uploader

--- frontend/src/components/SubmissionCreate/ImageUpload.js
+++ frontend/src/components/SubmissionCreate/ImageUpload.js
@@ -1,49 +1,20 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import React, { useState } from "react";
+import ImageUploader from "react-images-upload";
+import { Box, } from "@chakra-ui/react";
 
-export default function PaymentForm() {
+const ImageUpload = ({ setImages }) => {
   return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Payment method
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField required id="cardName" label="Name on card" fullWidth autoComplete="cc-name" />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField required id="expDate" label="Expiry date" fullWidth autoComplete="cc-exp" />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
-      </Grid>
-    </React.Fragment>
+      <Box w={"100%"}>
+        <ImageUploader
+          withIcon={true}
+          withPreview={true}
+          buttonText="Choose images to Upload"
+          onChange={setImages}
+          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+          maxFileSize={5242880}
+        />
+      </Box>
   );
-}
+};
+
+export default ImageUpload;
