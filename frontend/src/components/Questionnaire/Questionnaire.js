00ea90a2dcb3f8a2345455d53da76006de11bab8
Ayan Ahmad KCL 19002255 <ayan.ahmad@kcl.ac.uk>
2021-03-17 17:24:51 +0400

    Fix Formatting

--- frontend/src/components/Questionnaire/Questionnaire.js
+++ frontend/src/components/Questionnaire/Questionnaire.js
@@ -1,75 +1,49 @@
 import { useState } from "react";
-import {
-  Button,
-  Box,
-  Flex,
-  SimpleGrid,
-  Stack,
-} from "@chakra-ui/react";
+import { Button, Box, Flex, SimpleGrid, Stack } from "@chakra-ui/react";
 import Question from "./Question";
-import {
-  ArrowForwardIcon,
-  ArrowBackIcon,
-  CheckCircleIcon,
-} from "@chakra-ui/icons";
+import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
 
-// Questions will come from backend
 const questionsObject = [
   {
     questionID: 0,
     questionText: "In the past 7 days, have you felt unwell?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 1,
-    questionText: "In the past 7 days, have you had a fever (temperature higher than 36C)?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    questionText:
+      "In the past 7 days, have you had a fever (temperature higher than 36C)?",
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 2,
-    questionText: "In the past 7 days, have you seen redness around your ulcer?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    questionText:
+      "In the past 7 days, have you seen redness around your ulcer?",
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 3,
-    questionText: "In the past 7 days, have you seen any puss around your ulcer?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    questionText:
+      "In the past 7 days, have you seen any puss around your ulcer?",
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 4,
-    questionText: "In the past 7 days, has your ulcer been hotter to touch than usual?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    questionText:
+      "In the past 7 days, has your ulcer been hotter to touch than usual?",
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 5,
-    questionText: "In the past 7 days, has one foot been hotter to touch than the other?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    questionText:
+      "In the past 7 days, has one foot been hotter to touch than the other?",
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 6,
-    questionText: "In the past 7 days, have you noticed any unusual smells from the wound?",
-    answerOptions: [
-      { answerText: "No" },
-      { answerText: "Yes" },
-    ],
+    questionText:
+      "In the past 7 days, have you noticed any unusual smells from the wound?",
+    answerOptions: [{ answerText: "No" }, { answerText: "Yes" }],
   },
   {
     questionID: 7,
@@ -86,16 +60,10 @@ const Questionnaire = ({ answers, setAnswers }) => {
   const [questionNumber, setQuestionNumber] = useState(0);
 
   const onChangeOption = (optionIndex) => {
-    /*console.log(optionIndex)
-    let temp = answers;
-    temp[questionNumber] = {
-      question_id: questionNumber,
-      answer: optionIndex
-    };*/
     let temp = answers;
     temp.questionnaire[questionNumber] = optionIndex;
     setAnswers(temp);
-    console.log(answers)
+    console.log(answers);
   };
 
   return (
