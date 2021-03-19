04ca267823344864a1693cc819434ac3d8361bc5
zaini <alimzaini@outlook.com>
2021-03-19 20:03:03 +0000

    Refactor ReviewSubmissions

--- frontend/src/components/SubmissionsView/RequestCards/RequestCardsTable.js
+++ frontend/src/components/SubmissionsView/RequestCards/RequestCardsTable.js
@@ -1,49 +1,112 @@
-import { Container, Text } from "@chakra-ui/layout";
+import { Container, Text, Center, Heading } from "@chakra-ui/layout";
 import React from "react";
 import {
   Alert,
   AlertIcon,
   AlertDescription,
   AlertTitle,
+  Spinner,
 } from "@chakra-ui/react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import RequestCard from "./RequestCard";
 
-const RequestCardsTable = ({ data }) => {
+const RequestCardsTable = () => {
+  const { loading, data, error } = useQuery(GET_REQUESTS);
+
   let markup;
-  if (data.length === 0) {
+
+  if (loading) {
     markup = (
-      <Container>
-        <Alert
-          status="info"
-          variant="subtle"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          textAlign="center"
-          height="200px"
-        >
-          <AlertIcon boxSize="40px" mr={0} />
-          <AlertTitle mt={4} mb={1} fontSize="lg">
-            No fulfilled and unreviewed requests
-          </AlertTitle>
-          <AlertDescription maxWidth="sm">
-            Your patients have no new submissions which you have requested and
-            have not yet reviewed
-          </AlertDescription>
-        </Alert>
-      </Container>
+      <Center>
+        <Spinner size="xl" />
+      </Center>
+    );
+  } else if (error) {
+    markup = (
+      <Alert status="error">
+        <AlertIcon />
+        {error.graphQLErrors[0].message}
+      </Alert>
     );
   } else {
-    markup = (
-      <>
-        {data.map((e, i) => {
-          return <RequestCard key={i} data={e} />;
-        })}
-      </>
-    );
+    let data_rows = data.getRequestsForReview;
+    if (data_rows.length === 0) {
+      markup = (
+        <Container>
+          <Alert
+            status="info"
+            variant="subtle"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+            height="200px"
+          >
+            <AlertIcon boxSize="40px" mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize="lg">
+              No fulfilled and unreviewed requests
+            </AlertTitle>
+            <AlertDescription maxWidth="sm">
+              Your patients have no new submissions which you have requested and
+              have not yet reviewed
+            </AlertDescription>
+          </Alert>
+        </Container>
+      );
+    } else {
+      markup = (
+        <>
+          {data_rows.map((e, i) => {
+            return <RequestCard key={i} data={e} />;
+          })}
+        </>
+      );
+    }
   }
 
   return markup;
 };
 
 export default RequestCardsTable;
+
+const GET_REQUESTS = gql`
+  query getRequests {
+    getRequestsForReview {
+      id
+      type
+      deadline
+      fulfilled
+      Submission {
+        id
+        Images {
+          id
+          url
+        }
+        Answers {
+          id
+          Question {
+            id
+            text
+          }
+          value
+          extra
+        }
+        flag
+        createdAt
+      }
+      Patient {
+        id
+        fname
+        lname
+        email
+      }
+      Doctor {
+        id
+        fname
+        lname
+        email
+      }
+    }
+  }
+`;
