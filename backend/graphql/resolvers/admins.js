80f707cfda618c769ba6244e3b2d3c38bced865a
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-19 15:11:02 +0000

    Add superadmin constraint to admin creation and retrieval

--- backend/graphql/resolvers/admins.js
+++ backend/graphql/resolvers/admins.js
@@ -1,9 +1,29 @@
-const { UserInputError } = require("apollo-server-core");
-const { Admin, Hospital } = require("../../models/index.js");
+const { AuthenticationError, UserInputError } = require("apollo-server-core");
+const { SuperAdmin, Admin, Hospital } = require("../../models/index.js");
+
+const getAuthenticatedAdmin = async (context) => {
+  // Get the user based on the context
+  const user = isAuth(context);
+
+  // Make sure user is a super admin
+  if (user.accountType !== "SUPERADMIN") {
+    throw new AuthenticationError("Invalid user account type!");
+  }
+
+  // Make sure the super admin is 'real' (i.e. in the db)
+  const admin = await SuperAdmin.findByPk(user.id);
+  if (!admin) {
+    throw new AuthenticationError("Invalid user!");
+  }
+
+  return admin;
+};
 
 module.exports = {
   Query: {
-    getAdmins: async () => {
+    getAdmins: async (_, __, context) => {
+      const admin = getAuthenticatedAdmin(context);
+
       try {
         const admins = await Admin.findAll();
         return admins;
@@ -13,7 +33,9 @@ module.exports = {
     },
   },
   Mutation: {
-    createAdmin: async (_, user_details) => {
+    createAdmin: async (_, user_details, context) => {
+      const admin = getAuthenticatedAdmin(context);
+
       const hospital = await Hospital.findByPk(user_details.hospital_id);
 
       if (!hospital) {
