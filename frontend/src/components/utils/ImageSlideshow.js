de763b47cc1d613380521a8c62976eae67c1a6a5
zaini <alimzaini@outlook.com>
2021-03-19 00:34:34 +0000

    Add image slideshow component

---
+++ frontend/src/components/utils/ImageSlideshow.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import { Button, VStack, Text, Box, HStack } from "@chakra-ui/react";
+import ModalImage from "react-modal-image";
+
+const ImageSlideshow = () => {
+  const images = [
+    "https://i.imgur.com/EpQofxh.png",
+    "https://i.imgur.com/EsbdsaJ.png",
+    "https://i.imgur.com/lGqTIfq.png",
+  ];
+  const [index, setIndex] = useState(0);
+  const [imageComponents, setImageComponents] = useState([]);
+
+  useEffect(() => {
+    let temp = [];
+    images.forEach((e, i) => {
+      let component = (
+        <ModalImage
+          key={i}
+          small={e}
+          large={e}
+          alt="submission"
+          showRotate={true}
+        />
+      );
+
+      temp.push(component);
+    });
+    setImageComponents(temp);
+  }, []);
+
+  return (
+    <VStack>
+      {imageComponents[index]}
+      <HStack>
+        <Button isDisabled={index === 0} onClick={() => setIndex(index - 1)}>
+          Prev
+        </Button>
+        <Button
+          isDisabled={index === images.length - 1}
+          onClick={() => setIndex(index + 1)}
+        >
+          Next
+        </Button>
+      </HStack>
+      <Text fontWeight="bold">
+        {index + 1} / {images.length}
+      </Text>
+    </VStack>
+  );
+};
+
+export default ImageSlideshow;
