415d2c056c563326a321a40baefcb413ff57fcda
zaini <alimzaini@outlook.com>
2021-03-20 03:06:23 +0000

    Add headings to invite pages

--- frontend/src/pages/My/NewInvitePage.js
+++ frontend/src/pages/My/NewInvitePage.js
@@ -1,10 +1,16 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Center, Heading } from "@chakra-ui/react";
 import CreateInviteForm from "../../components/InviteForm/CreateInviteForm";
 
 // Page to create a new invite
 const NewInvitePage = () => {
   return (
     <Container pt="20px">
+      <Center>
+        <Heading>Create Invitation</Heading>
+      </Center>
+      <br />
+      <hr />
+      <br />
       <CreateInviteForm />
     </Container>
   );
