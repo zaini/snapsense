51f3229bc2d92ccf2b2d7aefc840db04c4ee8d37
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-20 23:53:36 +0000

    Remove unnecessary imports + comments

--- frontend/src/tests/HomeLayout.test.js
+++ frontend/src/tests/HomeLayout.test.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
 import HomeLayout from '../pages/Home/HomeLayout';
-import MainNavbar from "../components/utils/MainNavbar";
 
 test('renders without crashing', () => {
   const renderPage = render(<BrowserRouter><HomeLayout /></BrowserRouter>);
   expect(renderPage).toBeTruthy()
 })
-
-// test('Home layout renders the main navbar', () => {
-//   const { getByTestId } = render(<HomeLayout />);
-//   expect(within(HomeLayout).getByTestId('mainNavbar')).toBeTruthy();
-// });
 
 test('Home layout renders the main navbar', () => {
   const wrapper = render(<BrowserRouter><HomeLayout /></BrowserRouter>)
