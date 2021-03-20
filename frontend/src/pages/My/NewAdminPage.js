5aa2a154ce2f4efa06cfc3f97191a7bbf5374763
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-20 04:34:33 +0000

    Add admin functionality

---
+++ frontend/src/pages/My/NewAdminPage.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import {
+  Alert,
+  AlertIcon,
+  Center,
+  Container,
+  Heading,
+  Spinner,
+} from "@chakra-ui/react";
+
+import NewAdminForm from "../../components/Admin/NewAdminForm";
+
+const NewAdminPage = () => {
+  const params = useParams();
+
+  const {
+    loading,
+    data: { getSpecificHospital: hospital } = {},
+    error,
+  } = useQuery(GET_HOSPITAL, {
+    variables: { hospital_id: params.hospital_id },
+  });
+
+  let markup;
+
+  if (loading) {
+    markup = (
+      <Center>
+        <Spinner size="xl" />
+      </Center>
+    );
+  } else if (error) {
+    markup = (
+      <Alert status="error">
+        <AlertIcon />
+        {error.graphQLErrors[0].message}
+      </Alert>
+    );
+  } else {
+    markup = <NewAdminForm hospital={hospital} />;
+  }
+
+  return (
+    <Container>
+      <Heading textAlign="center">Create an Admin</Heading>
+      <br />
+      <hr />
+      <br />
+      {markup}
+    </Container>
+  );
+};
+
+export default NewAdminPage;
+
+const GET_HOSPITAL = gql`
+  query getSpecificHospital($hospital_id: ID!) {
+    getSpecificHospital(hospital_id: $hospital_id) {
+      id
+      name
+      contact_email
+    }
+  }
+`;
