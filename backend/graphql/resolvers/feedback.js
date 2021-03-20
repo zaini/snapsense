3caf2c80025d5835e1d14dd93522771952801b49
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-20 04:33:22 +0000

    Add feedback resolvers

--- backend/graphql/resolvers/feedback.js
+++ backend/graphql/resolvers/feedback.js
@@ -1,8 +1,31 @@
-const { Feedback } = require("../../models/index.js");
+const { AuthenticationError } = require("apollo-server");
+
+const { Feedback, SuperAdmin } = require("../../models/index.js");
+const isAuth = require("../../utils/isAuth.js");
+
+const getAuthenticatedSuperAdmin = async (context) => {
+  // Get the user based on the context
+  const user = isAuth(context);
+
+  // Make sure user is a super admin
+  if (user.accountType !== "SUPERADMIN") {
+    throw new AuthenticationError("Invalid user account type!");
+  }
+
+  // Make sure the super admin is 'real' (i.e. in the db)
+  const superAdmin = await SuperAdmin.findByPk(user.id);
+  if (!superAdmin) {
+    throw new AuthenticationError("Invalid user!");
+  }
+
+  return superAdmin;
+};
 
 module.exports = {
   Query: {
-    getFeedback: async () => {
+    getFeedback: async (_, __, context) => {
+      const superAdmin = await getAuthenticatedSuperAdmin(context);
+
       const feedback = await Feedback.findAll();
       return feedback;
     },
