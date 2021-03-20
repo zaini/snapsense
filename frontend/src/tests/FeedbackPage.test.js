c7baef919763e4c57f1785ace874c822c9a1fafa
mahsumkocabey <mahsumkocabey@hotmail.com>
2021-03-20 09:50:18 +0000

    Test feedbackpage render

--- frontend/src/tests/FeedbackPage.test.js
+++ frontend/src/tests/FeedbackPage.test.js
@@ -1,2 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import FeedbackPage from "../FeedbackPage";
+import { render, screen, cleanup } from "@testing-library/react";
+
+import FeedbackPage from "../pages/Home/FeedbackPage";
+
+test("render the page without crashing", () => {
+  render(<FeedbackPage />);
+});
+
+// ... more tests ...
