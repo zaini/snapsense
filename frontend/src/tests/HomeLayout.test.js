7f63045028448577817aff8c0e64674d162edaa2
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-20 23:45:26 +0000

    test if main navbar exists in home layout

--- frontend/src/tests/HomeLayout.test.js
+++ frontend/src/tests/HomeLayout.test.js
@@ -4,15 +4,19 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom'; 
 import HomeLayout from '../pages/Home/HomeLayout';
-
-// const ActualHomeLayout = jest.requireActual(HomeLayout);
-// jest.mock(HomeLayout, () =>
-//   <div data-testid="homeLayout">
-//     <HomeLayout />
-//   </div>
-// );
+import MainNavbar from "../components/utils/MainNavbar";
 
 test('renders without crashing', () => {
   const renderPage = render(<BrowserRouter><HomeLayout /></BrowserRouter>);
   expect(renderPage).toBeTruthy()
+})
+
+// test('Home layout renders the main navbar', () => {
+//   const { getByTestId } = render(<HomeLayout />);
+//   expect(within(HomeLayout).getByTestId('mainNavbar')).toBeTruthy();
+// });
+
+test('Home layout renders the main navbar', () => {
+  const wrapper = render(<BrowserRouter><HomeLayout /></BrowserRouter>)
+  expect(wrapper.findAllByRole('MainNavbar')).toBeTruthy()
 })
