1253a98e5e93ce74642ee2dd08ca9e3293ab617e
zaini <alimzaini@outlook.com>
2021-03-20 04:32:14 +0000

    Fix minor bug with null submissions

--- frontend/src/pages/My/RequestsPage.js
+++ frontend/src/pages/My/RequestsPage.js
@@ -182,10 +182,14 @@ const cols = [
     flex: 0.2,
     renderCell: ({ row }) => {
       const submission = row.Submission;
+      if (submission) {
+        return <p>{submission.id}</p>;
+      } else {
+        return <p>No Submission Made</p>;
+      }
       // const { fname, lname, email } = submission;
       // TODO instead of showing submission ID, show a button to view the actual submission
       // This button will depend on the account type you are logged in as
-      return <p>{submission.id || "No Submission Made"}</p>;
     },
   },
   {
