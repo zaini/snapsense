b25dbce3785774625051ba8b1d0d4497a92da4bc
Soyoung Park <p.soyoung63@gmail.com>
2021-03-22 02:57:26 +0900

    Need to add login for testing profilepage

--- frontend/src/tests/ProfilePage.test.js
+++ frontend/src/tests/ProfilePage.test.js
@@ -3,12 +3,25 @@ import { render } from "@testing-library/react";
 import ProfilePage from '../pages/My/ProfilePage';
 
 test('renders without crashing', () => {
+
+    // const email = '@gmail.com';
+    // const password = '';
+
+    // const wrapper = shallow(<ProfilePage handleLogin={state => {
+    //     expect(state.email).to.be.equal(email);
+    //     expect(state.password).to.be.equal(password);
+    // }} />);
+
+    // wrapper.setState({ email: '@gmail.com', password: ''});
+
+    // wrapper.find('button').simulate('click');
+
     const renderProfilePage = render(<ProfilePage />);
-    expect(renderProfilePage).toBeTruthy()
+    expect(renderProfilePage).toBeTruthy();
 })
 
 test('Profile page has a userInfo', () => {
-    const wrapper = render(<ProfilePage />)
-    const userInfo = wrapper.findByRole('UserInfo')
-    expect(userInfo).toBeTruthy()
+    const wrapper = render(<ProfilePage />);
+    const userInfo = wrapper.findByRole('UserInfo');
+    expect(userInfo).toBeTruthy();
 })
