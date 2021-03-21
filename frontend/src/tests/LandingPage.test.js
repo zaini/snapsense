e07ecc73d89eb99d0b08a1255c9a768ce5a98334
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-21 01:32:33 +0000

    Add comment to landing page test

--- frontend/src/tests/LandingPage.test.js
+++ frontend/src/tests/LandingPage.test.js
@@ -8,11 +8,12 @@ test('renders without crashing', () => {
   expect(renderLandingPage).toBeTruthy()
 })
 
+//Check to see if it contains more tests
 describe('Landing page renders banner of information', () => {
   const wrapper = render(<LandingPage/>)
-  const banner = wrapper.findAllByRole('Hero')
 
   test('Landing page has a banner', () => {
+    const banner = wrapper.findByRole('Hero')
     expect(banner).toBeTruthy()
   })
   
