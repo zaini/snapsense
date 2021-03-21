c78c62aef85fcab3b7cd7603ef86ed17093a7726
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-21 00:28:35 +0000

    Test rendering of main navbar itself

--- frontend/src/tests/MainNavbar.test.js
+++ frontend/src/tests/MainNavbar.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
 import MainNavbar from '../components/utils/MainNavbar';
+
+test('renders without crashing', () => {
+  const renderNavbar = render(<BrowserRouter><MainNavbar/></BrowserRouter>);
+  expect(renderNavbar).toBeTruthy()
+})
