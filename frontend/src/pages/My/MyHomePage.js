2958f3ea9cafc15d61038cf399ee8fe0bb21bd08
zaini <alimzaini@outlook.com>
2021-03-21 21:06:02 +0000

    Remove home from superadmin

--- frontend/src/pages/My/MyHomePage.js
+++ frontend/src/pages/My/MyHomePage.js
@@ -1,7 +1,9 @@
 import { Container } from "@chakra-ui/layout";
 import { Heading } from "@chakra-ui/react";
 
+// How useful analytics for patients and doctors
 const MyHomePage = () => {
+
   return (
     <Container>
       <Heading>My Page</Heading>
