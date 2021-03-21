0cf6e11880b6f6d09c158436ac8516c3ede05a10
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-21 00:51:54 +0000

    Add test for rendering

--- frontend/src/tests/LandingPage.test.js
+++ frontend/src/tests/LandingPage.test.js
@@ -1,2 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import LandingPage from "../LandingPage";
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import LandingPage from '../pages/Home/LandingPage';
+
+test('renders without crashing', () => {
+  const renderLandingPage = render(<LandingPage/>);
+  expect(renderLandingPage).toBeTruthy()
+})
