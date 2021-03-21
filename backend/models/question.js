23295ca4d5ded150efbe1c372031dec78ff3e8ca
alisherzhaken <alisherzhaken@gmail.com>
2021-03-21 16:20:18 +0000

    Add association

--- backend/models/question.js
+++ backend/models/question.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Question.hasMany(models.Answer, { foreignKey: "question_id" });
     }
   }
   Question.init(
