910e8a2c10b4a8cc641f7dc7a457ef53a88b3507
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-21 23:12:59 +0000

    Rename test for title check

--- frontend/src/tests/hero.test.js
+++ frontend/src/tests/hero.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { render, screen, queryByAttribute, getByTestId, getByAltText } from "@testing-library/react";
+import { render, screen, queryByAttribute, getByTestId, getByAltText, toBeInTheDocument} from "@testing-library/react";
 import LandingPage from '../pages/Home/LandingPage';
 import Hero from "../components/utils/Hero";
-
 
 const props = {}
 
@@ -10,6 +9,8 @@ const { getByText } = render(
     <LandingPage><Hero {...props} /></LandingPage>,
 )
 
-test('renders without crashing', () => {
-  expect(getByText('Snapsense, welcomes you to the future')).toBeInTheDocument()
+test('hero contains correct title', () => {
+  expect(getByText('Snapsense, welcomes you to the future')).toBeInTheDocument();
 })
+
+
