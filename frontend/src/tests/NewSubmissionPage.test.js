4ea7954e2466b8a1b7170e037f2a6d388e1585b8
benudall98 <bu12@outlook.com>
2021-03-22 22:44:56 +0000

    Add a rendering test

--- frontend/src/tests/NewSubmissionPage.test.js
+++ frontend/src/tests/NewSubmissionPage.test.js
@@ -2,4 +2,8 @@ import { render, screen, cleanup } from "@testing-library/react";
 
 import NewSubmissionPage from "../pages/My/NewSubmissionPage";
 
+test("render the page without crashing", () => {
+  const renderPage = render(<NewSubmissionPage/>);
+  expect(renderPage).toBeTruthy()
+ });
  
