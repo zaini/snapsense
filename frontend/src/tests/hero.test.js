36aee2882453250184c988db7e92fef6f737537e
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-22 13:47:07 +0000

    Rewrite tests to include all properties of the hero + add hero test id

--- frontend/src/tests/hero.test.js
+++ frontend/src/tests/hero.test.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import LandingPage from '../pages/Home/LandingPage';
+import { toHaveAttribute } from '@testing-library/jest-dom';
 import Hero from "../components/utils/Hero";
 
-const props = {}
-
-const { getByText } = render(
-    <LandingPage><Hero {...props} /></LandingPage>,
-)
-
 test('hero contains correct title', () => {
-  expect(getByText('Snapsense, welcomes you to the future')).toBeInTheDocument();
+  const landingPage = render(<LandingPage/>);
+  const heroTitle = landingPage.getByTestId('heroTitle');
+  expect(heroTitle.textContent).toBe('Snapsense, welcomes you to the future');
+})
+
+test('hero contains correct subtitle', () => {
+  const landingPage = render(<LandingPage/>);
+  const heroSubtitle = landingPage.getByTestId('heroSubtitle');
+  expect(heroSubtitle.textContent).toBe('Medical aid at the click of a button. Tele-medicine at its finest.');
+})
+
+test('hero contains correct image', () => {
+  const landingPage = render(<LandingPage />);
+  const heroImage = landingPage.queryByTestId('heroImage');
+  expect(heroImage).toBeTruthy();
 })
 
 test('the correct hero is rendered', () => {
-  const landPage = render(<LandingPage />)
-  const foundHero = landPage.findByTestId('landingHero')
+  const landingPage = render(<LandingPage/>);
+  const foundHero = landingPage.findByTestId('landingHero');
   expect(foundHero).toBeTruthy();
 })
 
