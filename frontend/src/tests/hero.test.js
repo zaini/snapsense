12167a0d4c12145998d1b949bc91154fe86a5959
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-22 00:20:38 +0000

    Remove unused imports

--- frontend/src/tests/hero.test.js
+++ frontend/src/tests/hero.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, queryByAttribute, getByTestId, getByAltText, toBeInTheDocument} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import LandingPage from '../pages/Home/LandingPage';
 import Hero from "../components/utils/Hero";
 
