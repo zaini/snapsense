a7e6377f8788447fecfffef6dee4a6c2e109a19d
mahsumkocabey <mahsumkocabey@hotmail.com>
2021-03-24 01:34:22 +0000

    Star rate test

--- frontend/src/tests/FeedbackPage.test.js
+++ frontend/src/tests/FeedbackPage.test.js
@@ -1,7 +1,8 @@
 import { CREATE_FEEDBACK, Feedback } from "../components/Feedback/Feedback";
 import { MockedProvider } from "@apollo/client/testing";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, screen } from "@testing-library/react";
 
+//Creating mock data
 const mocks = [
   {
     request: {
@@ -19,35 +20,60 @@ const mocks = [
   },
 ];
 
+//Component variable to avoid code duplication.
 const component = render(
   <MockedProvider mocks={mocks} addTypename={false}>
     <Feedback />
   </MockedProvider>
 );
 
-/*------ Tests  -----*/
+/* Essentials */
+screen.debug(); //to see the html elements
+afterEach(cleanup);
 
+/*------ Tests  -----*/
+//Feedback page general tests
 describe("FeedbackPage", () => {
+  it("has those written lines", () => {
+    expect(
+      screen.getByText("How would you rate your experience with SnapSense?")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Additional Feedback")).toBeInTheDocument();
+    expect(screen.getByText("Submit")).toBeInTheDocument();
+  });
+
   it("renders without crashing", () => {
     expect(component).toBeTruthy();
   });
 });
 
-describe("Click submit button", () => {
-  const button = component.findAllByRole("Button");
+//Submit button check
+describe("Submit button", () => {
+  const button = component.findAllByTestId("submitbutton");
 
-  it("Landing page has a button", () => {
+  it("Page has a submit button", () => {
     expect(button).toBeTruthy();
   });
 
   it("On submit", () => {
     expect(button.innerHTML).toBe(undefined);
-    // fireEvent.click(button);
-    // expect(btn.innerHTML).toBe("You Clicked");
   });
 });
 
-it("Textarea", () => {
-  const TextArea = component.findAllByRole("TextArea");
-  expect(TextArea).toBeTruthy();
+//Input text test
+describe("Feedback textbox", () => {
+  const TextArea = component.findAllByTestId("textarea");
+
+  it("Textarea exist", () => {
+    expect(TextArea).toBeTruthy();
+  });
+});
+
+//Star Rate test
+describe("Star Rating", () => {
+  const StarRate = component.findAllByTestId("starrate");
+
+  it("Start rating exist", () => {
+    expect(StarRate).toBeTruthy();
+  });
 });
