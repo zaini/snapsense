a23d0b1e6132dc87291f972c94e6da21177b3063
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-26 18:16:45 +0000

    Add tests for getPatientAsDoctor query

--- backend/tests/graphql/patients.spec.js
+++ backend/tests/graphql/patients.spec.js
@@ -2,23 +2,145 @@ const request = require("supertest");
 
 const app = require("../../index");
 
-test("allPatients", async (done) => {
-  const response = await request(app).post("/graphql").send({
-    query: `
-            query {
-                getHospitals{
-                  id
-                  name
-                  contact_email
-                }
-              }
-            `,
+describe("patient resolvers", () => {
+  test("get a patient as a doctor where the patient belongs to the doctor", async (done) => {
+    const loginResponse = await request(app).post("/graphql").send({
+      query: `
+				mutation {
+					login(
+						email: "doctor1@nhs.net"
+						password: "Password123"
+						account_type: "DOCTOR"
+					)	
+					{
+						accessToken
+					}
+				}
+			`,
+    });
+
+    const {
+      data: {
+        login: { accessToken },
+      },
+    } = loginResponse.body;
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({
+        query: `
+			query {
+				getPatientAsDoctor(patient_id: "1") {
+					id
+					fname
+					lname
+					email
+				}
+			}
+		`,
+      })
+      .set("authorization", `Bearer ${accessToken}`);
+
+    const { body } = response;
+
+    expect(body).toMatchObject({
+      data: {
+        getPatientAsDoctor: {
+          id: "1",
+          fname: "Patient",
+          lname: "One",
+          email: "patient1@gmail.com",
+        },
+      },
+    });
+    done();
   });
-  const { body } = response;
-  expect(body).toMatchObject({
-    data: {
-      getHospitals: [],
-    },
+
+  test("get a patient as a doctor where the patient does not belongs to the doctor", async (done) => {
+    const loginResponse = await request(app).post("/graphql").send({
+      query: `
+				mutation {
+					login(
+						email: "doctor1@nhs.net"
+						password: "Password123"
+						account_type: "DOCTOR"
+					)	
+					{
+						accessToken
+					}
+				}
+			`,
+    });
+
+    const {
+      data: {
+        login: { accessToken },
+      },
+    } = loginResponse.body;
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({
+        query: `
+			query {
+				getPatientAsDoctor(patient_id: "2") {
+					id
+					fname
+					lname
+					email
+				}
+			}
+		`,
+      })
+      .set("authorization", `Bearer ${accessToken}`);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch("Invalid patient!");
+    done();
   });
-  done();
+
+	test("get a patient as a doctor where the patient does not exist", async (done) => {
+    const loginResponse = await request(app).post("/graphql").send({
+      query: `
+				mutation {
+					login(
+						email: "doctor1@nhs.net"
+						password: "Password123"
+						account_type: "DOCTOR"
+					)	
+					{
+						accessToken
+					}
+				}
+			`,
+    });
+
+    const {
+      data: {
+        login: { accessToken },
+      },
+    } = loginResponse.body;
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({
+        query: `
+			query {
+				getPatientAsDoctor(patient_id: "100") {
+					id
+					fname
+					lname
+					email
+				}
+			}
+		`,
+      })
+      .set("authorization", `Bearer ${accessToken}`);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch("Invalid patient!");
+    done();
+  });
 });
