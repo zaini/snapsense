40e013994e0e43d299a8278312eff17216372743
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-27 19:41:31 +0000

    Add loggedin test for questionnaire retrieval

--- backend/tests/graphql/questions.spec.js
+++ backend/tests/graphql/questions.spec.js
@@ -6,14 +6,52 @@ describe("questions resolvers", () => {
   test("should not get questionnaire if not logged in", async (done) => {
     const response = await request(app).post("/graphql").send({
       query: `
-		query {
-			getQuestionnaire {
-			id
-			text
-			}
-		}
-	  `,
+				query {
+					getQuestionnaire {
+					id
+					text
+					}
+				}
+	  	`,
     });
+
+    const errorMessage = response.body.errors[0].message;
+    expect(errorMessage).toMatch("Missing Authorization Header");
+    done();
+  });
+
+  test("should get questionnaire if logged in", async (done) => {
+    // Login
+    const patientOneToken = await request(app).post("/graphql").send({
+      query: `
+				mutation {
+					login(
+						email: "patient1@gmail.com"
+						password: "Password123"
+						account_type: "PATIENT"
+					)	
+					{
+						accessToken
+					}
+				}
+			`,
+    });
+
+    patientOneToken = patientOneToken.body.data.login.accessToken;
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({
+        query: `
+					query {
+						getQuestionnaire {
+						id
+						text
+						}
+					}
+				`,
+      })
+      .set("authorization", `Bearer ${patientOneToken}`);
 
     const errorMessage = response.body.errors[0].message;
     expect(errorMessage).toMatch("Missing Authorization Header");
