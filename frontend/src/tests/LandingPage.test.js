79ff7d8c6e2bf3416832a1b90730802edbb41c57
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-27 18:34:58 +0000

    Format code with prettier

--- frontend/src/tests/LandingPage.test.js
+++ frontend/src/tests/LandingPage.test.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 import { render } from "@testing-library/react";
-import LandingPage from '../pages/Home/LandingPage';
+import LandingPage from "../pages/Home/LandingPage";
 
-test('renders without crashing', () => {
-  const renderLandingPage = render(<LandingPage/>);
-  expect(renderLandingPage).toBeTruthy()
-})
+test("renders without crashing", () => {
+  const renderLandingPage = render(<LandingPage />);
+  expect(renderLandingPage).toBeTruthy();
+});
 
-describe('Landing page renders hero of information', () => {
-  const wrapper = render(<LandingPage/>)
+describe("Landing page renders hero of information", () => {
+  const wrapper = render(<LandingPage />);
 
-  test('Landing page has a hero', () => {
-    const hero = wrapper.findByRole('Hero')
-    expect(hero).toBeTruthy()
-  })
-  
-})
+  test("Landing page has a hero", () => {
+    const hero = wrapper.findByRole("Hero");
+    expect(hero).toBeTruthy();
+  });
+});
--- frontend/src/tests/hero.test.js
+++ frontend/src/tests/hero.test.js
@@ -1,36 +1,31 @@
-import React from 'react';
+import React from "react";
 import { render } from "@testing-library/react";
-import LandingPage from '../pages/Home/LandingPage';
+import LandingPage from "../pages/Home/LandingPage";
 
-test('the correct hero is rendered', () => {
-  const landingPage = render(<LandingPage/>);
-  const foundHero = landingPage.findByTestId('landingHero');
+test("the correct hero is rendered", () => {
+  const landingPage = render(<LandingPage />);
+  const foundHero = landingPage.findByTestId("landingHero");
   expect(foundHero).toBeTruthy();
-})
+});
 
-describe('hero displays correct information', () => {
-
-  test('hero contains correct title', () => {
-    const landingPage = render(<LandingPage/>);
-    const heroTitle = landingPage.getByTestId('heroTitle');
-    expect(heroTitle.textContent).toBe('Snapsense, welcomes you to the future');
-  })
-  
-  test('hero contains correct subtitle', () => {
-    const landingPage = render(<LandingPage/>);
-    const heroSubtitle = landingPage.getByTestId('heroSubtitle');
-    expect(heroSubtitle.textContent).toBe('Medical aid at the click of a button. Tele-medicine at its finest.');
-  })
-  
-  test('hero contains correct image', () => {
+describe("hero displays correct information", () => {
+  test("hero contains correct title", () => {
     const landingPage = render(<LandingPage />);
-    const heroImage = landingPage.queryByTestId('heroImage');
+    const heroTitle = landingPage.getByTestId("heroTitle");
+    expect(heroTitle.textContent).toBe("Snapsense, welcomes you to the future");
+  });
+
+  test("hero contains correct subtitle", () => {
+    const landingPage = render(<LandingPage />);
+    const heroSubtitle = landingPage.getByTestId("heroSubtitle");
+    expect(heroSubtitle.textContent).toBe(
+      "Medical aid at the click of a button. Tele-medicine at its finest."
+    );
+  });
+
+  test("hero contains correct image", () => {
+    const landingPage = render(<LandingPage />);
+    const heroImage = landingPage.queryByTestId("heroImage");
     expect(heroImage).toBeTruthy();
-  })
-
-})
-
-
-
-
-
+  });
+});
