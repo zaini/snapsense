3c3395c51ee2a7a5cf5bda6df517fe5e0be1cda6
benudall98 <bu12@outlook.com>
2021-03-27 12:49:31 +0000

    fix mocks

--- frontend/src/tests/NewSubmissionPage1.test.js
+++ frontend/src/tests/NewSubmissionPage1.test.js
@@ -2,17 +2,18 @@ import TestRenderer from 'react-test-renderer';
 import { MockedProvider } from '@apollo/client/testing';
 import { gql, useMutation } from "@apollo/client";
 import { UPLOAD_SUBMISSION, NewSubmissionPage } from "../pages/My/NewSubmissionPage";
+import { render, cleanup, screen } from "@testing-library/react";
 
 
-const mocks = 
+const mocks = [
     {
       request: {
         query: UPLOAD_SUBMISSION,
         variables: { id: 1, Patient: 1, images: 1 , answers: 1 },
-      }
-    };
-
-const component = TestRenderer.create(
+      },
+    }];
+    //Render component
+    const component = render(
     <MockedProvider mocks={mocks} addTypename={false}>
       <NewSubmissionPage />
     </MockedProvider>,
