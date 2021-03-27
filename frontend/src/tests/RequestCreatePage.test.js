b04da76a0bf5379c6aca45638ed8740c945f93d3
Ayan Ahmad, HP SPECTRE <ayanahmad5701@gmail.com>
2021-03-27 13:41:05 +0400

    Add New Test

--- frontend/src/tests/RequestCreatePage.test.js
+++ frontend/src/tests/RequestCreatePage.test.js
@@ -23,6 +23,24 @@ const GET_PATIENT_AS_DOCTOR = gql`
   }
 `;
 
+const CREATE_REQUEST = gql`
+  mutation createRequest(
+    $patient_id: ID!
+    $request_type: Int!
+    $deadline: String!
+    $interval: Int!
+    $frequency: Int!
+  ) {
+    createRequest(
+      patient_id: $patient_id
+      request_type: $request_type
+      deadline: $deadline
+      frequency: $frequency
+      interval: $interval
+    )
+  }
+`;
+
 const mocks = [
   {
     request: {
@@ -45,10 +63,23 @@ const mocks = [
     request: {
       query: GET_PATIENT_AS_DOCTOR,
       variables: {
-        patient_id: 2,
+        patient_id: "2",
       },
     },
     error: Error("This is an invalid patient"),
+  },
+  {
+    request: {
+      query: CREATE_REQUEST,
+      variables: {
+        patient_id: "1",
+        request_type: 3,
+        interval: 0,
+        frequency: 0,
+        deadline: new Date().getTime().toString(),
+      },
+    },
+    result: { data: { createRequest: true } },
   },
 ];
 
@@ -153,38 +184,32 @@ test("interval on schedule form on request page allows values within 0 and 20 in
     const interval = within(form).getByTestId("inputInterval");
     fireEvent.change(interval, { target: { value: "15" } });
     expect(interval.value).toBe("15");
-
   });
 });
 
-// test("form submit gives necessary validations", async () => {
-//   setup();
+test("form submit gives success message", async () => {
+  setup();
 
-//   expect(screen.getByText(/Loading/i)).toBeInTheDocument();
-//   await waitFor(() => {
-//     fireEvent(
-//       screen.getByText("Scheduled"),
-//       new MouseEvent("click", {
-//         bubbles: true,
-//         cancelable: true,
-//       })
-//     );
-//     expect(screen.getByTestId("periodicForm")).toBeInTheDocument();
-//     const form = screen.getByTestId("periodicForm");
 
-//     const interval = within(form).getByTestId("inputInterval");
-//     fireEvent.change(interval, { target: { value: 15 } });
-//     expect(interval.value).toBe(15);
+  expect(screen.getByText(/Loading/i)).toBeInTheDocument();
+  await waitFor(async () => {
+    expect(screen.getByTestId("nonPeriodicForm")).toBeInTheDocument();
+  });
 
-//     const btnSubmit = within(form).getByTestId("formSubmit");
-//     fireEvent(
-//       btnSubmit,
-//       new MouseEvent("click", {
-//         bubbles: true,
-//         cancelable: true,
-//       })
-//     );
+  const form = screen.getByTestId("nonPeriodicForm");
 
-//     within(form).getByText(/Please select a value that is no more that 20/i);
-//   });
-// });
+  const datePickerContainer = within(form).getByTestId("DatePickerContainer");
+  const datePicker = datePickerContainer.childNodes[0];
+  datePicker.selected = new Date("2022-03-25").getTime().toString();
+  console.log(datePicker);
+  
+
+  const btnSubmit = within(form).getByText("Submit");
+  fireEvent.click(btnSubmit);
+  await waitFor(async () => {
+    expect(screen.getByTestId("formSubmitInnerLoader")).toBeInTheDocument();
+  });
+  // await waitFor(async () => {
+  //   expect(screen.getByTestId("formSubmitInnerSuccess")).toBeInTheDocument();
+  // });
+});
