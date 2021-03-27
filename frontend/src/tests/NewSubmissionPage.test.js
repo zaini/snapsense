8cac1d6f84442adc903022d748e099b1db3a853d
benudall98 <bu12@outlook.com>
2021-03-27 14:33:43 +0000

    delete unnecessary test files

--- frontend/src/tests/NewSubmissionPage.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, cleanup } from "@testing-library/react";
-
-import NewSubmissionPage from "../pages/My/NewSubmissionPage";
-
-test("render the page without crashing", () => {
-  const renderPage = render(<NewSubmissionPage/>);
-  expect(renderPage).toBeTruthy()
- });
- 
