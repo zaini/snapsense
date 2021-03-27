d542a699a2cfe8bdb401b5bd1d7ba201c2950668
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-03-27 19:50:56 +0000

    Refactor test

--- backend/tests/graphql/questions.spec.js
+++ backend/tests/graphql/questions.spec.js
@@ -22,7 +22,7 @@ describe("questions resolvers", () => {
 
   test("should get questionnaire if logged in", async (done) => {
     // Login
-    const patientOneToken = await request(app).post("/graphql").send({
+    const loginRes = await request(app).post("/graphql").send({
       query: `
 				mutation {
 					login(
@@ -37,7 +37,7 @@ describe("questions resolvers", () => {
 			`,
     });
 
-    patientOneToken = patientOneToken.body.data.login.accessToken;
+    const patientOneToken = loginRes.body.data.login.accessToken;
 
     const response = await request(app)
       .post("/graphql")
@@ -53,8 +53,50 @@ describe("questions resolvers", () => {
       })
       .set("authorization", `Bearer ${patientOneToken}`);
 
-    const errorMessage = response.body.errors[0].message;
-    expect(errorMessage).toMatch("Missing Authorization Header");
+    const { body } = response;
+
+    expect(body).toMatchObject(questionnaireObject);
+
     done();
   });
 });
+
+const questionnaireObject = {
+  data: {
+    getQuestionnaire: [
+      { id: "1", text: "In the past 7 days, have you felt unwell?" },
+      {
+        id: "2",
+        text:
+          "In the past 7 days, have you had a fever (temperature higher than 36C)?",
+      },
+      {
+        id: "3",
+        text: "In the past 7 days, have you seen redness around your ulcer?",
+      },
+      {
+        id: "4",
+        text: "In the past 7 days, have you seen any puss around your ulcer?",
+      },
+      {
+        id: "5",
+        text:
+          "In the past 7 days, has your ulcer been hotter to touch than usual?",
+      },
+      {
+        id: "6",
+        text:
+          "In the past 7 days, has one foot been hotter to touch than the other?",
+      },
+      {
+        id: "7",
+        text:
+          "In the past 7 days, have you noticed any unusual smells from the wound?",
+      },
+      {
+        id: "8",
+        text: "Please add any other notes for your clinician (optional):",
+      },
+    ],
+  },
+};
