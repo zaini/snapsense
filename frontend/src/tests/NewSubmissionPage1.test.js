74692a0e45e533786d41810e88d2a59962046173
mahsumkocabey <mahsumkocabey@hotmail.com>
2021-03-27 14:28:21 +0000

    Fix conflicts

--- frontend/src/tests/NewSubmissionPage1.test.js
+++ frontend/src/tests/NewSubmissionPage1.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { MockedProvider } from "@apollo/client/testing";
 import { gql, useMutation } from "@apollo/client";
 import {
@@ -32,36 +31,3 @@ it("should be truthy", () => {
   const button = component.findAllByTestId("submitbutton");
   expect(button).toBeTruthy();
 });
-=======
-import { MockedProvider } from '@apollo/client/testing';
-import { gql, useMutation } from "@apollo/client";
-import UPLOAD_SUBMISSION, {NewSubmissionPage} from "../pages/My/NewSubmissionPage";
-import { render, cleanup, screen } from "@testing-library/react";
-
-const mocks = [
-    {
-      request: {
-        query: UPLOAD_SUBMISSION,
-
-      },
-    },
-];
-
-    //Render component
-    const component = render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <NewSubmissionPage />
-    </MockedProvider>,
-    );
-
-    /* Essential */
-    afterEach(async () => {
-        await cleanup();
-    });
-  
-
-    it("should be truthy", () => {
-        const button = component.findAllByTestId("submitbutton");
-        expect(button).toBeTruthy();
-    });
->>>>>>> d9413db2fcda5851e040c24dcf7b9088bf3de5bf
