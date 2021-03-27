d9413db2fcda5851e040c24dcf7b9088bf3de5bf
benudall98 <bu12@outlook.com>
2021-03-27 14:02:12 +0000

    remove import

--- frontend/src/tests/NewSubmissionPage1.test.js
+++ frontend/src/tests/NewSubmissionPage1.test.js
@@ -1,25 +1,32 @@
-import TestRenderer from 'react-test-renderer';
 import { MockedProvider } from '@apollo/client/testing';
 import { gql, useMutation } from "@apollo/client";
-import { UPLOAD_SUBMISSION, NewSubmissionPage } from "../pages/My/NewSubmissionPage";
+import UPLOAD_SUBMISSION, {NewSubmissionPage} from "../pages/My/NewSubmissionPage";
 import { render, cleanup, screen } from "@testing-library/react";
-
 
 const mocks = [
     {
       request: {
         query: UPLOAD_SUBMISSION,
-        variables: { id: 1, Patient: 1, images: 1 , answers: 1 },
+        // variables: { id: '1', Patient: {id:'1', fname:'firstname', lname:'lname',email:'patient1@gmail.com'}, Images: {id:'1',url:''}, 
+        // Answers: {id:'1', Question: {id: '1', text:'Text Question'} }},
       },
-    }];
+    },
+];
+
     //Render component
     const component = render(
     <MockedProvider mocks={mocks} addTypename={false}>
       <NewSubmissionPage />
     </MockedProvider>,
-);
+    );
 
-it("should be truthy", () => {
-    const button = component.findAllByTestId("submitbutton");
-    expect(button).toBeTruthy();
-});
+    /* Essential */
+    afterEach(async () => {
+        await cleanup();
+    });
+  
+
+    it("should be truthy", () => {
+        const button = component.findAllByTestId("submitbutton");
+        expect(button).toBeTruthy();
+    });
