4a94db5ff9963f3945df2780dfd2751b62d7fab1
Ayan Ahmad, HP SPECTRE <ayanahmad5701@gmail.com>
2021-03-28 22:05:45 +0400

    Add test scaffold for Screening Page

--- frontend/src/tests/ReviewSubmissionPage.test.js
+++ frontend/src/tests/ReviewSubmissionPage.test.js
@@ -10,39 +10,32 @@ import {
 import { MockedProvider } from "@apollo/client/testing";
 import { act } from "react-dom/test-utils";
 
-import NewRequestPage from "../pages/My/NewRequestPage";
 import { Route, MemoryRouter } from "react-router";
+import ReviewSubmissions from "../pages/My/ReviewSubmissions";
 
 afterEach(cleanup);
 
-const { CREATE_REQUEST } = require("../components/Request/NewRequestForm");
-const { GET_PATIENT_AS_DOCTOR } = require("../pages/My/NewRequestPage");
+const {
+  GET_REQUESTS,
+} = require("../components/SubmissionsView/RequestCards/RequestCardsTable");
+const {
+  GET_SUBMISSIONS,
+} = require("../components/SubmissionsView/SubmissionCards/SubmissionCardsTable");
 
-const initialDate = new Date();
 const mocks = [
   {
     request: {
-      query: GET_PATIENT_AS_DOCTOR,
-      variables: {
-        patient_id: "1",
-      },
+      query: GET_REQUESTS,
+      variables: {},
     },
     result: {
-      data: {
-        getPatientAsDoctor: {
-          id: 1,
-          fname: "Patient",
-          lname: "One",
-        },
-      },
+      data: {},
     },
   },
   {
     request: {
-      query: GET_PATIENT_AS_DOCTOR,
-      variables: {
-        patient_id: "2",
-      },
+      query: GET_REQUESTS,
+      variables: {},
     },
     error: {
       graphQLErrors: [
@@ -52,31 +45,97 @@ const mocks = [
       ],
     },
   },
-  {
-    request: {
-      query: CREATE_REQUEST,
-      variables: {
-        patient_id: 1,
-        request_type: 3,
-        interval: 0,
-        frequency: 0,
-        deadline: initialDate.getTime().toString(),
-      },
-    },
-    result: { data: { createRequest: true } },
-  },
 ];
 
 const setup = async () => {
   act(() => {
     render(
       <MockedProvider mocks={mocks} addTypename={false}>
-        <MemoryRouter initialEntries={["/my/patients/1/requests/new"]}>
-          <Route path="/my/patients/:patient_id/requests/new">
-            <NewRequestPage dateIn={initialDate} />
+        <MemoryRouter initialEntries={["/my/submissions/review"]}>
+          <Route path="/my/submissions/review">
+            <ReviewSubmissions />
           </Route>
         </MemoryRouter>
       </MockedProvider>
     );
   });
 };
+
+describe("page loading", () => {
+  test("page renders without crashing", async () => {
+    expect(setup).toBeTruthy();
+  });
+
+  test("correct heading is displayed when page is loaded", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("correct tabs are displayed when page is loaded", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+});
+
+describe("fulfilled and unreviewed tab", () => {
+  test("spinner shows up when tab is loaded", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("number of cards displayed are correct", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("buttons are in the correct state", async () => {
+    //disbled and enabled
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("correct number of images are displayed", async () => {
+    // check button is disabled
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("correct number of answers and notes are displayed", async () => {
+    // check button is disabled
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("accurate patient and submission information is loaded", async () => {
+    // check button is disabled
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("submit button submits form", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("view submission redirect to correct page", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("make a new request redirects to correct page", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+});
+
+describe("fulfilled and unreviewed tab", () => {
+  test("spinner shows up when tab is loaded", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+
+  test("number of cards displayed are correct", async () => {
+    setup();
+    // expect(screen.getByText(/aaaaaaaaaaaaaaaaa/i)).toBeInTheDocument();
+  });
+});
