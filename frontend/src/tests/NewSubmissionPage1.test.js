9822592900317ce72c513cf492264fd0edd296c4
benudall98 <bu12@outlook.com>
2021-03-28 20:43:40 +0100

    add mutation test

--- frontend/src/tests/NewSubmissionPage1.test.js
+++ frontend/src/tests/NewSubmissionPage1.test.js
@@ -1,20 +1,51 @@
 import { MockedProvider } from "@apollo/client/testing";
 import { gql, useMutation } from "@apollo/client";
-import {
-  UPLOAD_SUBMISSION,
-  NewSubmissionPage,
-} from "../pages/My/NewSubmissionPage";
+import { NewSubmissionPage } from "../pages/My/NewSubmissionPage";
 
 import { render, fireEvent, cleanup, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+afterEach(cleanup);
+
+const { UPLOAD_SUBMISSION } = require("../pages/My/NewSubmissionPage");
+const {GET_SUBMISSION}= require("../components/SubmissionsView/SubmissionsComponent.js")
+
+
 const mocks = [
   {
-    request: {
-      query: UPLOAD_SUBMISSION,
-    },
+    
+      request: {
+        query: UPLOAD_SUBMISSION,
+        variables: {
+          submission_id: "1",
+          image: {id: '1', url:''},
+          answers: {
+            id: '1', Question: {id: '1', text: 'Text question' }, value: '', extra: ''},
+          }
+        },
+      result: { 
+        data: {
+           uploadSubmission: true } 
+          },
   },
+  // {
+  //   request: {
+  //     query: GET_SUBMISSION,
+  //     variables: {
+  //       patient_id: "2",
+  //     },
+  //   },
+  //   error: {
+  //     graphQLErrors: [
+  //       {
+  //         message: "This is an invalid patient",
+  //       },
+  //     ],
+  //   },
+  // },
+
 ];
+
 
 /*------ Essential  -----*/
 afterEach(async () => {
