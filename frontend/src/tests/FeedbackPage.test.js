a9bdfa0ba9602d6d9c1560a81457e1bc159e7cff
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-29 19:59:19 +0100

    Remove empty test files + unnecessary comments

--- frontend/src/tests/FeedbackPage.test.js
+++
@@ -1,2 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import FeedbackPage from "../pages/Home/FeedbackPage";
--- frontend/src/tests/MainNavbar.test.js
+++ frontend/src/tests/MainNavbar.test.js
@@ -7,9 +7,3 @@ test('renders without crashing', () => {
   const renderNavbar = render(<BrowserRouter><MainNavbar/></BrowserRouter>);
   expect(renderNavbar).toBeTruthy()
 })
-
-// test('renders a navbar of items', () => {
-//   const { getByRole } = render(<BrowserRouter><MainNavbar /></BrowserRouter>);
-//   // const navbarOfItems = renderNavbar.getByTestId('navbarItems');
-//   expect(getByRole('NavbarMenuItems')).toBeTruthy();
-// })
