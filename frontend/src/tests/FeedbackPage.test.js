6236069ad4277729eaddea90687876ea50ceea9d
mahsumkocabey <mahsumkocabey@hotmail.com>
2021-03-29 23:16:00 +0100

    Refactor code, test feedback submission

--- frontend/src/tests/FeedbackPage.test.js
+++ frontend/src/tests/FeedbackPage.test.js
@@ -1,36 +1,55 @@
-import { MockedProvider } from "@apollo/client/testing";
-import { render, cleanup, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import Feedback, { CREATE_FEEDBACK } from "../components/Feedback/Feedback";
-
-//Creating mock data
-const mocks = [
-  {
-    request: {
-      query: CREATE_FEEDBACK,
-    },
-  },
-];
+import { MockedProvider } from "@apollo/client/testing";
+import {
+  render,
+  cleanup,
+  screen,
+  waitFor,
+  fireEvent,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 //Render component
 const component = render(
-  <MockedProvider mocks={mocks} addTypename={false}>
+  <MockedProvider
+    customResolvers={{
+      Mutation: () => ({
+        createFeedback: () => ({ id: 1, stars: 5, extra: "go to the store" }),
+      }),
+    }}
+  >
     <Feedback />
   </MockedProvider>
 );
 
 /* Essential */
-afterEach(cleanup);
+afterEach(async () => {
+  await cleanup();
+});
 
 /*------ Tests  -----*/
 // Feedback page general tests
+describe("A user can input in textarea to make a feedback", () => {
+  userEvent.type(
+    screen.getByTestId("text-area"),
+    "The website was really useful and easy to use. Therefore I do not need to visit the hospital. Thanks!"
+  );
+  expect(screen.getByTestId("text-area")).toHaveValue(
+    "The website was really useful and easy to use. Therefore I do not need to visit the hospital. Thanks!"
+  );
+});
+
 describe("FeedbackPage", () => {
-  it("has those written lines", () => {
-    expect(
-      screen.getByText("How would you rate your experience with SnapSense?")
-    ).toBeInTheDocument();
-    expect(screen.getByText("Additional Feedback")).toBeInTheDocument();
-    expect(screen.getByText("Submit")).toBeInTheDocument();
+  it("A user can submit a written feedback", async () => {
+    const additionalFeedback = screen.getByPlaceholderText("Enter here");
+    const submitButton = component.getByTestId("submit-button");
+
+    fireEvent.change(additionalFeedback, {
+      target: { value: "Hello World" },
+    });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => screen.getByText("Hello World"));
   });
 
   it("renders without crashing", () => {
@@ -38,45 +57,26 @@ describe("FeedbackPage", () => {
   });
 });
 
-//Submit button check
+//Submit button exist
 describe("Submit button", () => {
   it("Page has a submit button", () => {
-    const button = component.findAllByTestId("submitbutton");
+    const button = component.findAllByTestId("submit-button");
     expect(button).toBeTruthy();
   });
 });
 
-//TODO
-// describe("User can click on submit button", () => {
-//   const SubmitButton = jest.fn();
-//   userEvent.click(screen.getByText("Submit"));
-//   expect(SubmitButton).toBeCalledTimes(1);
-// });
-
-//Input text test
+//Input text exist
 describe("Feedback textbox", () => {
   it("Textarea exist", () => {
-    const TextArea = component.findAllByTestId("textarea");
+    const TextArea = component.findAllByTestId("text-area");
     expect(TextArea).toBeTruthy();
   });
 });
 
-describe("A user can input in textarea to make a feedback", () => {
-  userEvent.type(
-    screen.getByTestId("textarea"),
-    "The website was really useful and easy to use. Therefore I do not need to visit the hospital. Thanks!"
-  );
-  expect(screen.getByTestId("textarea")).toHaveValue(
-    "The website was really useful and easy to use. Therefore I do not need to visit the hospital. Thanks!"
-  );
-});
-
-
-
-//Star Rate test
+//Star Rate exist
 describe("Star Rating", () => {
   it("Start rating exist", () => {
-    const StarRate = component.findAllByTestId("starrate");
+    const StarRate = component.findAllByTestId("star-rate");
     expect(StarRate).toBeTruthy();
   });
 });
