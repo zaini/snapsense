8e946083d8da92ccdc2195f3552a4551466b783e
Era Mullahasani Dula <eradula01@hotmail.com>
2021-03-29 15:14:07 +0100

    Redo + mock data for AuthContext

--- frontend/src/tests/NavbarMenuItems.test.js
+++ frontend/src/tests/NavbarMenuItems.test.js
@@ -3,34 +3,22 @@ import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
 import NavbarMenuItems from '../components/utils/NavbarMenuItems';
 import MainNavbar from '../components/utils/MainNavbar';
-// import { AuthContext } from "../context/auth";
+import HomeLayout from '../pages/Home/HomeLayout';
+import { AuthContext } from "../context/auth";
+import { Route} from "react-router";
 
-// test('renders without crashing', () => {
-//   const renderNavbarItems = render(<BrowserRouter><NavbarMenuItems/></BrowserRouter>);
-//   expect(renderNavbarItems).toBeTruthy()
-// })
-
-const AuthContext = createContext();
-// Setup mock
-const AuthProvider = ({ children }) => {
+const admin = {
+  user: {accountType: "ADMIN"}
+};
   
-}
-// 
+const doctor = {
+  user: {accountType: "DOCTOR"}
+};
 
-test("mock hook", () => {
-    // useContextMock.mockReturnValue(mockData);
-  const {getByTestId} = render(
-    // <AuthContext.Provider>
-      <AuthContext.Consumer value={{
-        user: {accountType: 'ADMIN'},
-        login: () => {},
-        logout: () => {},
-      }}>
-        {() => {
-          <BrowserRouter><MainNavbar /></BrowserRouter>
-        }}
-      </AuthContext.Consumer>
-    // </AuthContext.Provider>
-  );
-  expect(getByTestId('myDash').textContent).toEqual('My Dashboard');
-});
+const patient = {
+  user: {accountType: "PATIENT"}
+};
+
+const notLoggedIn = {
+  user: {accountType: ""}
+};
