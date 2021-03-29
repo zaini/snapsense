64afe629337391b545cd66810aaa97e1a0d0d538
Soyoung Park <p.soyoung63@gmail.com>
2021-03-29 22:34:17 +0900

    Remove test-id in submissionsComponent

--- frontend/src/components/SubmissionsView/SubmissionsComponent.js
+++ frontend/src/components/SubmissionsView/SubmissionsComponent.js
@@ -32,7 +32,7 @@ const SubmissionsComponent = () => {
     );
   } else {
     const dataRows = data.getSubmissions;
-    markup = <SubmissionsViewSwitch data-testid="submissionComponent" data={dataRows} />;
+    markup = <SubmissionsViewSwitch data={dataRows} />;
   }
 
   return markup;
