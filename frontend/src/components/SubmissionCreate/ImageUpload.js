07846c972c5ca8ecd6fc2e8086cfd0a5abeb4993
benudall98 <bu12@outlook.com>
2021-04-02 17:29:40 +0100

    Fix import

--- frontend/src/components/SubmissionCreate/ImageUpload.js
+++ frontend/src/components/SubmissionCreate/ImageUpload.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import ImageUploader from "react-images-upload";
-import { Box, } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const ImageUpload = ({ setImages }) => {
   return (
-      <Box w={"100%"}>
-        <ImageUploader
-          withIcon={true}
-          withPreview={true}
-          buttonText="Choose images to Upload"
-          onChange={setImages}
-          imgExtension={[".jpg", ".gif", ".png", ".gif"]}
-          maxFileSize={5242880}
-        />
-      </Box>
+    <Box w={"100%"}>
+      <ImageUploader
+        className="ImageUpload"
+        withIcon={true}
+        withPreview={true}
+        buttonText="Choose images to Upload"
+        onChange={setImages}
+        imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+        maxFileSize={5242880}
+      />
+    </Box>
   );
 };
 
