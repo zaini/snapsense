3aeeeea46f58f274602d536a7e1a736339ecd8df
Ayan Ahmad, HP SPECTRE <ayanahmad5701@gmail.com>
2021-04-04 23:50:21 +0400

    Refactor Mocks ProfilePage

--- frontend/src/tests/ProfilePage.test.js
+++ frontend/src/tests/ProfilePage.test.js
@@ -12,95 +12,7 @@ import { Route, MemoryRouter } from "react-router";
 import { AuthContext } from "../context/auth";
 import { MockedProvider } from "@apollo/client/testing";
 
-
-const {
-  GET_SUBMISSIONS,
-} = require("../components/SubmissionsView/SubmissionsComponent");
-const { CHANGE_PASSWORD } = require("../components/utils/ChangePasswordModal");
-const { DELETE_ACCOUNT } = require("../components/utils/DeleteAccountModal");
-
-const Mock = [
-  {
-    request: {
-      query: GET_SUBMISSIONS,
-      variables: {
-        patient_id: "1",
-      },
-    },
-    result: {
-      data: {
-        getSubmissions: [
-          {
-            id: "1",
-            flag: "High Risk",
-            createdAt: "1234567890",
-            Patient: {
-              id: "1",
-              fname: "first",
-              lname: "last",
-              email: "patient1@gmail.com",
-              password: "Password123",
-              flag: "asdfghjk",
-            },
-            Images: {
-              id: "1",
-              url: "asdfghjk",
-            },
-            Answers: {
-              id: "1",
-              Question: {
-                id: "1",
-                text: "asdfghj",
-              },
-            },
-            value: "1",
-            extra: "sdfghj",
-          },
-        ],
-      },
-    },
-  },
-  {
-    request: {
-      query: CHANGE_PASSWORD,
-      variables: {
-        password: "asd",
-        password_confirmation: "asd",
-      },
-    },
-    error: { message: "Validation error: Invalid password" },
-  },
-  {
-    request: {
-      query: CHANGE_PASSWORD,
-      variables: {
-        password: "Password000",
-        password_confirmation: "Password000",
-      },
-    },
-    result: { data: { changePassword: true } },
-  },
-  {
-    request: {
-      query: DELETE_ACCOUNT,
-      variables: {
-        password: "WRONGPASSWORD",
-        password_confirmation: "WRONGPASSWORD",
-      },
-    },
-    error: { message: "Incorrect password!" },
-  },
-  {
-    request: {
-      query: DELETE_ACCOUNT,
-      variables: {
-        password: "Password123",
-        password_confirmation: "Password123",
-      },
-    },
-    response: { data: { deleteAccount: true } },
-  },
-];
+import Mock from "./mocks/profilePageMocks";
 
 afterEach(cleanup);
 
@@ -350,5 +262,4 @@ describe("delete account modal tests", () => {
 
     expect(screen.getByText("Incorrect password!")).toBeInTheDocument();
   });
-
 });
