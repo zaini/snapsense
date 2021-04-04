f84699ef6019376928ebb1cbfc9a3b4a9502cd8c
Ayan Ahmad, HP SPECTRE <ayanahmad5701@gmail.com>
2021-04-04 23:49:01 +0400

    Refactor Mocks HospitalPage

--- frontend/src/tests/HospitalPage.test.js
+++ frontend/src/tests/HospitalPage.test.js
@@ -13,42 +13,7 @@ import { Route, MemoryRouter } from "react-router";
 
 afterEach(cleanup);
 
-const { GET_HOSPITAL } = require("../pages/My/HospitalPage");
-
-const mocks = [
-  {
-    request: {
-      query: GET_HOSPITAL,
-      variables: {
-        hospital_id: "1",
-      },
-    },
-    result: {
-      data: {
-        getSpecificHospital: {
-          id: "1",
-          name: "Hospital One",
-          contact_email: "hospital.one@gmail.com",
-        },
-      },
-    },
-  },
-  {
-    request: {
-      query: GET_HOSPITAL,
-      variables: {
-        hospital_id: "2",
-      },
-    },
-    error: {
-      graphQLErrors: [
-        {
-          message: "Hospital does not exist",
-        },
-      ],
-    },
-  },
-];
+import mocks from "./mocks/hospitalPageMocks";
 
 //Render component
 const setup = () => {
