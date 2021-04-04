f41e3c583f2ecd65bd676e0ae335890730ec4f9f
Ayan Ahmad, HP SPECTRE <ayanahmad5701@gmail.com>
2021-04-04 23:49:11 +0400

    Refactor Mocks LoginPage

--- frontend/src/tests/LoginPage.test.js
+++ frontend/src/tests/LoginPage.test.js
@@ -12,7 +12,7 @@ import {
 import { AuthContext } from "../context/auth";
 
 import LoginPage from "../pages/Home/LoginPage.js";
-import { LOGIN_USER, LoginForm } from "../components/Login/LoginForm";
+import mocks from "./mocks/loginPageMocks";
 
 afterEach(cleanup);
 
@@ -22,39 +22,7 @@ afterEach(cleanup);
 // type selector renders and works
 
 //Mock data for GQL
-const mocks = [
-  {
-    request: {
-      query: LOGIN_USER,
-      variables: {
-        email: "patient@gmail.com",
-        password: "ABCPassword123.",
-        account_type: "PATIENT",
-      },
-    },
-    error: {
-      message: "Invalid Credentials",
-    },
-  },
-  {
-    request: {
-      query: LOGIN_USER,
-      variables: {
-        email: "patient1@gmail.com",
-        password: "ABCPassword123.",
-        account_type: "PATIENT",
-      },
-    },
-    response: {
-      data: {
-        login: {
-          accessToken:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZm5hbWUiOiJQYXRpZW50IiwibG5hbWUiOiJPbmUiLCJlbWFpbCI6InBhdGllbnQxQGdtYWlsLmNvbSIsImZsYWciOjIsImNyZWF0ZWRBdCI6IjIwMjEtMDMtMjNUMTc6MzI6MzcuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjEtMDMtMjNUMTk6NDA6MDcuMDAwWiIsImFjY291bnRUeXBlIjoiUEFUSUVOVCIsImlhdCI6MTYxNjkyMTA5MywiZXhwIjoxNjE2OTI4MjkzfQ.LsXTQUJ7G80OtcyMRdt6F0dXIGq0H3lOvPf4oBzADdc",
-        },
-      },
-    },
-  },
-];
+
 //Render component
 const setup = () => {
   act(() => {
