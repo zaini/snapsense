39c2ad7613ef8f4ec135667fb6ed45f045048d17
Ayan Ahmad, HP SPECTRE <ayanahmad5701@gmail.com>
2021-04-05 13:13:20 +0400

    Fix imports

--- frontend/src/tests/mocks/adminPageMocks.js
+++ frontend/src/tests/mocks/adminPageMocks.js
@@ -1,50 +1,50 @@
-const { GET_ADMIN } = require("../../pages/My/AdminPage");
-const { DELETE_ADMIN } = require("../../components/Admin/DeleteAdminModal");
+import { GET_ADMIN } from "../../pages/My/AdminPage";
+import { DELETE_ADMIN } from "../../components/Admin/DeleteAdminModal";
 
 const mocks = [
-    {
-      request: {
-        query: GET_ADMIN,
-        variables: {
-          admin_id: "1",
-        },
+  {
+    request: {
+      query: GET_ADMIN,
+      variables: {
+        admin_id: "1",
       },
-      result: {
-        data: {
-          getAdminById: {
-            id: "1",
-            fname: "Admin",
-            lname: "One",
-            email: "admin1@gmail.com",
-            Hospital: {
-              name: "Hospital",
-            },
+    },
+    result: {
+      data: {
+        getAdminById: {
+          id: "1",
+          fname: "Admin",
+          lname: "One",
+          email: "admin1@gmail.com",
+          Hospital: {
+            name: "Hospital",
           },
         },
       },
     },
-    {
-      request: {
-        query: GET_ADMIN,
-        variables: {
-          admin_id: "2",
+  },
+  {
+    request: {
+      query: GET_ADMIN,
+      variables: {
+        admin_id: "2",
+      },
+    },
+    error: {
+      graphQLErrors: [
+        {
+          message: "Admin does not exist",
         },
-      },
-      error: {
-        graphQLErrors: [
-          {
-            message: "Admin does not exist",
-          },
-        ],
-      },
+      ],
     },
-    {
-      request: {
-        query: DELETE_ADMIN,
-        variables: { admin_id: "1" },
-      },
-      result: { data: { deleteAdmin: true } },
+  },
+  {
+    request: {
+      query: DELETE_ADMIN,
+      variables: { admin_id: "1" },
     },
-  ];
+    result: { data: { deleteAdmin: true } },
+  },
+];
 
-  export default mocks;
+export default mocks;
