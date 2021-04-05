1b492b04a338c73078cc286bdd2c8cce143f8642
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-05 12:27:28 +0100

    Add tests for getSubmissions as a patient

--- backend/tests/graphql/resolvers/submissions.spec.js
+++ backend/tests/graphql/resolvers/submissions.spec.js
@@ -4,6 +4,33 @@ const app = require("../../../index");
 
 let doctorOneToken, doctorTwoToken, patientOneToken, patientTwoToken;
 
+const getSubmissions = (authToken, patientId) => {
+  return request(app)
+    .post("/graphql")
+    .send({
+      query: `
+			query {
+				getSubmissions${patientId ? `(patient_id: ${patientId})` : ""} {
+					flag
+					Patient {
+						email
+					}
+					Answers {
+						Question {
+							text
+						}
+						value
+					}
+					Images {
+						url
+					}
+				}
+			}		
+		`,
+    })
+    .set("authorization", `Bearer ${authToken}`);
+};
+
 describe("submissions resolvers", () => {
   beforeAll(async (done) => {
     const {
@@ -19,4 +46,83 @@ describe("submissions resolvers", () => {
 
     done();
   });
+
+  it("should get submissions as a logged in patient", async (done) => {
+    const response = await getSubmissions(patientOneToken);
+    const { body } = response;
+    expect(body).toMatchObject(patientOneSubmissions);
+    done();
+  });
 });
+
+const patientOneSubmissions = {
+  data: {
+    getSubmissions: [
+      {
+        flag: 1,
+        Patient: { email: "patient1@gmail.com" },
+        Answers: [
+          {
+            Question: { text: "In the past 7 days, have you felt unwell?" },
+            value: true,
+          },
+          {
+            Question: {
+              text:
+                "In the past 7 days, have you had a fever (temperature higher than 36C)?",
+            },
+            value: true,
+          },
+          {
+            Question: {
+              text:
+                "In the past 7 days, have you seen redness around your ulcer?",
+            },
+            value: false,
+          },
+          {
+            Question: {
+              text:
+                "In the past 7 days, have you seen any puss around your ulcer?",
+            },
+            value: false,
+          },
+          {
+            Question: {
+              text:
+                "In the past 7 days, has your ulcer been hotter to touch than usual?",
+            },
+            value: true,
+          },
+          {
+            Question: {
+              text:
+                "In the past 7 days, has one foot been hotter to touch than the other?",
+            },
+            value: true,
+          },
+          {
+            Question: {
+              text:
+                "In the past 7 days, have you noticed any unusual smells from the wound?",
+            },
+            value: true,
+          },
+
+          {
+            Question: {
+              text: "Please add any other notes for your clinician (optional):",
+            },
+            value: true,
+          },
+        ],
+        Images: [
+          {
+            url:
+              "https://snapsensebucket.s3.ap-south-1.amazonaws.com/f150d94e-25cb-4973-bf26-d987b5bde188.jpg",
+          },
+        ],
+      },
+    ],
+  },
+};
