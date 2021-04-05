d137a9c48fdd52ed10ef7ba654ef133fc1c175dd
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-04-05 11:11:56 +0100

    Add tests for checkInvitation resolver

--- backend/tests/graphql/resolvers/inviteLink.spec.js
+++ backend/tests/graphql/resolvers/inviteLink.spec.js
@@ -52,4 +52,208 @@ describe("invite links resolvers", () => {
 
     done();
   });
+
+	it("should return correct token if checkInvitation is called with a valid invite for a patient", async (done) => {
+    const token = inviteToken(
+      "doctor1@nhs.net",
+      "newpatient@gmail.com",
+      "PATIENT",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const {
+      data: { checkInvitation: result },
+    } = response.body;
+
+    const decodedResponse = verify(result, ACCESS_TOKEN_SECRET_KEY);
+
+    // Make sure the decoded response contains the correct data, cannot check for full object equality due to timestamps
+    expect(decodedResponse).toEqual(
+      expect.objectContaining({
+        accountExists: false,
+        accountType: "PATIENT",
+        inviterEmail: "doctor1@nhs.net",
+        newAccountEmail: "newpatient@gmail.com",
+      })
+    );
+
+    done();
+  });
+
+  it("should correctly update accountExists field in the invitation token if patient exists", async (done) => {
+    const token = inviteToken(
+      "doctor1@nhs.net",
+      "patient2@gmail.com",
+      "PATIENT",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const {
+      data: { checkInvitation: result },
+    } = response.body;
+
+    const decodedResponse = verify(result, ACCESS_TOKEN_SECRET_KEY);
+
+    // Make sure the decoded response contains the correct data, cannot check for full object equality due to timestamps
+    expect(decodedResponse).toEqual(
+      expect.objectContaining({
+        accountExists: true, // accountExists should be set to true because "patient2@gmail.com" is an existing account
+        accountType: "PATIENT",
+        inviterEmail: "doctor1@nhs.net",
+        newAccountEmail: "patient2@gmail.com",
+      })
+    );
+
+    done();
+  });
+
+  it("should throw an error if checkInvitation is called with an invalid invite due to a deleted doctor", async (done) => {
+    const token = inviteToken(
+      "doctor10@nhs.net",
+      "newpatient@gmail.com",
+      "PATIENT",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch("Inviter no longer exists!");
+
+    done();
+  });
+
+  it("should throw an error if checkInvitation is called with an invite where the doctor and patient are already linked", async (done) => {
+    const token = inviteToken(
+      "doctor1@nhs.net",
+      "patient1@gmail.com",
+      "PATIENT",
+      true
+    );
+
+    const response = await checkInvitation(token);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch(
+      "You are already registered with this doctor!"
+    );
+
+    done();
+  });
+
+  it("should return correct token if checkInvitation is called with a valid invite for a doctor", async (done) => {
+    const token = inviteToken(
+      "admin1@gmail.com",
+      "newdoctor@nhs.net",
+      "DOCTOR",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const {
+      data: { checkInvitation: result },
+    } = response.body;
+
+    const decodedResponse = verify(result, ACCESS_TOKEN_SECRET_KEY);
+
+    // Make sure the decoded response contains the correct data, cannot check for full object equality due to timestamps
+    expect(decodedResponse).toEqual(
+      expect.objectContaining({
+        accountExists: false,
+        accountType: "DOCTOR",
+        inviterEmail: "admin1@gmail.com",
+        newAccountEmail: "newdoctor@nhs.net",
+      })
+    );
+
+    done();
+  });
+
+  it("should throw an error if checkInvitation is called with an invalid invite due to a deleted admin", async (done) => {
+    const token = inviteToken(
+      "admin10@gmail.com",
+      "newdoctor@nhs.net",
+      "DOCTOR",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch("Inviter no longer exists!");
+
+    done();
+  });
+
+  it("should throw an error if checkInvitation is called with an invalid invite due to the doctor being already signed up", async (done) => {
+    const token = inviteToken(
+      "admin1@gmail.com",
+      "doctor2@nhs.net",
+      "DOCTOR",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch("You already have an account!");
+
+    done();
+  });
+
+  it("should throw an error if checkInvitation is called with an invalid account type", async (done) => {
+    const token = inviteToken(
+      "admin1@gmail.com",
+      "doctor2@nhs.net",
+      "DOCTORS",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const errorMessage = response.body.errors[0].message;
+
+    expect(errorMessage).toMatch("Invalid Account Type!");
+
+    done();
+  });
+
+  it("should return invite token with emails lower cased if invite token is valid but contains upper-cased emails", async (done) => {
+    const token = inviteToken(
+      "AdMIN1@GMAIL.COM",
+      "NeWDOcTOR@nHs.NEt",
+      "DOCTOR",
+      false
+    );
+
+    const response = await checkInvitation(token);
+
+    const {
+      data: { checkInvitation: result },
+    } = response.body;
+
+    const decodedResponse = verify(result, ACCESS_TOKEN_SECRET_KEY);
+
+    // Make sure the decoded response contains the correct data with lower-cased emails
+    // Cannot check for full object equality due to timestamps
+    expect(decodedResponse).toEqual(
+      expect.objectContaining({
+        accountExists: false,
+        accountType: "DOCTOR",
+        inviterEmail: "admin1@gmail.com",
+        newAccountEmail: "newdoctor@nhs.net",
+      })
+    );
+
+    done();
+  });
 });
