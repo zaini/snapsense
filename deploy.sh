ae119dd101f247995afe851cade30aa13ce1eb7e
ayanahmad571 <72255062+ayanahmad571@users.noreply.github.com>
2021-04-06 19:54:14 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 eval `ssh-agent -s`
 ssh-add /home/ubuntu/keys/id_snap_gmail
-git pull
-sudo systemctl restart nginx
 pm2 stop server
-cd /home/ubuntu/snapsense
+git pull
 git checkout main
 git pull
 cd backend
 npm install
-git stash
 npm run db:setup
 git stash
 pm2 restart server
+cd /home/ubuntu/snapsense_production
+git pull 
+sudo systemctl restart nginx
